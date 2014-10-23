ba5549c53b47f53c93d5266309dfb84573aac664
Shaojun Lu <shaojun.lu@desy.de>
2014-10-23 13:37:03 +0000

    implement the TPC Endplate Services with hard code number. Todo: remove the hardcode, and read them from parameters XML file next step.

--- detector/other/SServices00.cpp
+++ detector/other/SServices00.cpp
@@ -38,6 +38,20 @@ using namespace DD4hep::Geometry;
 
 bool BuildTPCEndplateServices(PlacedVolume *pVol,Assembly *envelope)
 {
+  //Todo: change the hard code numbers, and read them from parameters XML file.
+  Material copper;
+  double TPC_Ecal_Hcal_barrel_halfZ=235.0;
+  double tpcEndplateServicesRing_R[8] = {45.0, 65.0, 85.0, 105.0, 125.0, 145.0, 165.0, 175.0};
+  double tpcEndplateServicesRing_ro[8] = {0.1555, 0.1555, 0.1555, 0.1555, 0.1555, 0.1555, 0.2985, 0.2453};
+  for(int i=0;i<8;i++){
+    Torus solidTube(tpcEndplateServicesRing_R[i], 0, tpcEndplateServicesRing_ro[i], 0, 2*M_PI);
+    double z_position = TPC_Ecal_Hcal_barrel_halfZ + tpcEndplateServicesRing_ro[i];
+    Volume EnvTube("service_tube",solidTube,copper);
+    Position pos1(0,0,z_position);
+    *pVol = envelope->placeVolume(EnvTube,pos1);
+    Position pos2(0,0,-z_position);
+    *pVol = envelope->placeVolume(EnvTube,pos2);
+  } 
 
   return true;
 }
@@ -87,7 +101,7 @@ static Ref_t create_element(LCDD& lcdd, xml_h element, SensitiveDetector sens)  
 //====================================================================
   cout << "\nBuilding SServices00"<< endl;
 
-  //BuildTPCEndplateServices(&pv,&envelope_assembly);
+  BuildTPCEndplateServices(&pv,&envelope_assembly);
 
   //BuildEcalBarrelServices(&pv,&envelope_assembly);
 
@@ -104,6 +118,7 @@ static Ref_t create_element(LCDD& lcdd, xml_h element, SensitiveDetector sens)  
 
   pv = motherVol.placeVolume(envelope_assembly);
   pv.addPhysVolID("system", det_id);
+  sdet.setVisAttributes( lcdd, x_det.visStr(),  envelope_assembly);
   sdet.setPlacement(pv);
 
   return sdet;
