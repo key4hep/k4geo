6642b6d33316481eda0f6ef09f8a3c73a5747795
Shaojun Lu <shaojun.lu@desy.de>
2014-10-23 11:16:24 +0000

    initialize a DD4hep service driver, and start to port Mokka ILD_o1_v05 SServices00.

---
+++ detector/other/SServices00.cpp
@@ -0,0 +1,74 @@
+//====================================================================
+//  DDSim - LC simulation based on DD4hep 
+//--------------------------------------------------------------------
+// DD4hep Geometry driver for SService00
+// Ported from Mokka
+//--------------------------------------------------------------------
+//  S.Lu, DESY
+//  $Id:$
+//====================================================================
+//
+//*******************************************************
+//*                                                     *
+//*                      Mokka                          * 
+//*   - the detailed geant4 simulation for ILC   -      *
+//*                                                     *
+//* For more information about Mokka, visit the         *
+//*                                                     *
+//*  Mokka.in2p3.fr  Mokka home page.                   *
+//*                                                     *
+//*******************************************************
+//
+// $Id: SServices00.cc,v 1.1 2009/06/04 11:40:42 musat Exp $
+// $Name: mokka-07-00 $
+//
+// 
+//
+// SServices00.cc
+//
+
+#include "DD4hep/DetFactoryHelper.h"
+#include "DD4hep/DD4hepUnits.h"
+#include "XMLHandlerDB.h"
+ 
+using namespace std;
+using namespace DD4hep;
+using namespace dd4hep;
+using namespace DD4hep::Geometry;
+
+
+
+static Ref_t create_element(LCDD& lcdd, xml_h element, SensitiveDetector sens)  {
+
+  static double tolerance = 0e0;
+
+  xml_det_t x_det = element;
+  string det_name = x_det.nameStr();
+  
+  int det_id = x_det.id();
+  DetElement sdet (det_name,det_id);
+  Volume motherVol = lcdd.pickMotherVolume(sdet);
+  
+  Assembly envelope_assembly( det_name + "assembly" ) ;
+  PlacedVolume pv;
+
+
+//====================================================================
+// build all services
+//====================================================================
+  cout << "\nBuilding SServices00"<< endl;
+
+
+
+//====================================================================
+// Place services into the world volume
+//====================================================================
+
+  pv = motherVol.placeVolume(envelope_assembly);
+  pv.addPhysVolID("system", det_id);
+  sdet.setPlacement(pv);
+
+  return sdet;
+}
+
+DECLARE_DETELEMENT(SServices00,create_element)
