e7a021b6096c72f9ddaaf5532867c707fa4e8ce2
Shaojun Lu <shaojun.lu@desy.de>
2014-11-12 10:37:13 +0000

    use the class from SServices00 header file to build TPC cooling services, and remove the hardcode values, and now read them from parameters XML as Mokka from DB.

--- detector/other/SServices00.cpp
+++ detector/other/SServices00.cpp
@@ -30,12 +30,15 @@
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/DD4hepUnits.h"
 #include "XMLHandlerDB.h"
+
+#include "SServices00.h"
  
 using namespace std;
 using namespace DD4hep;
 using namespace dd4hep;
 using namespace DD4hep::Geometry;
 
+/*
 bool BuildTPCEndplateServices(PlacedVolume *pVol,Assembly *envelope)
 {
   //Todo: change the hard code numbers, and read them from parameters XML file.
@@ -79,7 +82,7 @@ bool BuildSitCables(PlacedVolume *pVol,Assembly *envelope)
 
   return true;
 }
-
+*/
 
 static Ref_t create_element(LCDD& lcdd, xml_h element, SensitiveDetector sens)  {
 
@@ -101,7 +104,52 @@ static Ref_t create_element(LCDD& lcdd, xml_h element, SensitiveDetector sens)  
 //====================================================================
   cout << "\nBuilding SServices00"<< endl;
 
-  BuildTPCEndplateServices(&pv,&envelope_assembly);
+  //==================================================
+  //BuildTPCEndplateServices(&pv,&envelope_assembly);
+  //==================================================
+  
+  // start to prepare the Material and geometry as Mokka
+  Material copper;
+  double TPC_Ecal_Hcal_barrel_halfZ =lcdd.constant<double>("TPC_Ecal_Hcal_barrel_halfZ");
+
+  const int MAX_TPC_RINGS = 8;
+  double tpcEndplateServices_R[MAX_TPC_RINGS];
+  double tpcEndplateServices_r[MAX_TPC_RINGS];
+
+  tpcEndplateServices_R[0] = lcdd.constant<double>("tpcEndplateServicesRing1_R");
+  tpcEndplateServices_r[0] = lcdd.constant<double>("tpcEndplateServicesRing1_ro");
+
+  tpcEndplateServices_R[1] = lcdd.constant<double>("tpcEndplateServicesRing2_R");
+  tpcEndplateServices_r[1] = lcdd.constant<double>("tpcEndplateServicesRing2_ro");
+
+  tpcEndplateServices_R[2] = lcdd.constant<double>("tpcEndplateServicesRing3_R");
+  tpcEndplateServices_r[2] = lcdd.constant<double>("tpcEndplateServicesRing3_ro");
+
+  tpcEndplateServices_R[3] = lcdd.constant<double>("tpcEndplateServicesRing4_R");
+  tpcEndplateServices_r[3] = lcdd.constant<double>("tpcEndplateServicesRing4_ro");
+
+  tpcEndplateServices_R[4] = lcdd.constant<double>("tpcEndplateServicesRing5_R");
+  tpcEndplateServices_r[4] = lcdd.constant<double>("tpcEndplateServicesRing5_ro");
+
+  tpcEndplateServices_R[5] = lcdd.constant<double>("tpcEndplateServicesRing6_R");
+  tpcEndplateServices_r[5] = lcdd.constant<double>("tpcEndplateServicesRing6_ro");
+
+  tpcEndplateServices_R[6] = lcdd.constant<double>("tpcEndplateServicesRing7_R");
+  tpcEndplateServices_r[6] = lcdd.constant<double>("tpcEndplateServicesRing7_ro");
+
+  tpcEndplateServices_R[7] = lcdd.constant<double>("tpcEndplateServicesRing8_R");
+  tpcEndplateServices_r[7] = lcdd.constant<double>("tpcEndplateServicesRing8_ro");
+  
+  // start to build TPC cooling rings with the class BuildTPCEndplateServices
+  BuildTPCEndplateServices TPCEndplateServices;
+  TPCEndplateServices.setMaterial(copper);
+  TPCEndplateServices.sethalfZ(TPC_Ecal_Hcal_barrel_halfZ);
+  for(int i=0;i<MAX_TPC_RINGS;i++)
+    TPCEndplateServices.settpcEndplateServicesRing_R_ro(tpcEndplateServices_R[i],tpcEndplateServices_r[i]);
+  
+  TPCEndplateServices.DoBuildTPCEndplateServices(&pv,&envelope_assembly);
+ 
+  //BuildTPCEndplateServices(&pv,&envelope_assembly);
 
   //BuildEcalBarrelServices(&pv,&envelope_assembly);
 
