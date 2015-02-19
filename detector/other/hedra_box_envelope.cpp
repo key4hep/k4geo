a71152ff0a7342fb5f6ebb682531b47b1e59a91d
Shaojun Lu <shaojun.lu@desy.de>
2015-02-19 09:09:19 +0000

    Added two new envelopes. hedra_box_envelope is the envelope has polyhedra outer shape with box hole in the center, for example, ILD Ecal/Hcal Endcaps; tube_hedra_envelope is the envelope has circle outer shape with polyhedra hole in the center, for example, ILD Hcal Barrel.

---
+++ detector/other/hedra_box_envelope.cpp
@@ -0,0 +1,71 @@
+//====================================================================
+//  AIDA Detector description implementation
+//--------------------------------------------------------------------
+//  DD4hep Geometry polyhedra outer with box hole inner envelope
+//--------------------------------------------------------------------
+//  S.Lu, DESY
+//  $Id:$
+//====================================================================
+#include "DD4hep/DetFactoryHelper.h"
+#include "XML/Layering.h"
+
+using namespace std;
+using namespace DD4hep;
+using namespace DD4hep::Geometry;
+
+static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector /*sens*/)  {
+  static double tolerance = 0e0;
+  Layering      layering (e);
+  xml_det_t     x_det     = e;
+ //unused:  Material      air       = lcdd.air();
+
+  int           det_id    = x_det.id();
+  string        det_name  = x_det.nameStr();
+
+  xml_comp_t    x_dim     = x_det.dimensions();
+  int           nsides    = x_dim.numsides();
+
+ //unused:  double        dphi      = (2*M_PI/nsides);
+ //unused:  double        hphi      = dphi/2;
+
+ //unused:  double        thickness = layering.totalThickness();
+
+  double        inner_r   = x_dim.rmin();
+  double        outer_r   = x_dim.rmax();
+
+  //assert(thickness < (outer_r - inner_r));
+  
+  Material      env_mat   = lcdd.material(x_dim.materialStr());
+
+  xml_comp_t    env_pos     = x_det.position();
+  xml_comp_t    env_rot     = x_det.rotation();
+
+  Position      pos(env_pos.x(),env_pos.y(),env_pos.z());
+  RotationZYX   rotZYX(env_rot.z(),env_rot.y(),env_rot.x());
+
+  Transform3D   tr(rotZYX,pos);
+
+  RotationZYX   rotZYX_box(-env_rot.z(),env_rot.y(),env_rot.x());
+
+ //unused:  xml_comp_t    x_staves  = x_det.staves();
+
+
+  DetElement    sdet      (det_name,det_id);
+  Volume        motherVol = lcdd.pickMotherVolume(sdet);
+
+  // The shape PolyhedraRegular for envelope. You may define your shape.
+  PolyhedraRegular hedra_solid (nsides,0,outer_r+tolerance*2e0,x_dim.zhalf()*2.0);
+  Box              box_hole(inner_r+tolerance*2e0, inner_r+tolerance*2e0,x_dim.zhalf()+1.0e-5);
+  SubtractionSolid hedrabox(hedra_solid,box_hole,rotZYX_box);
+  Volume        envelope  (det_name+"_envelope",hedrabox,env_mat);
+ //unused:  PlacedVolume  env_phv   = 
+  motherVol.placeVolume(envelope,tr);
+
+
+  // Then build and place the layers into the envelope in your way here.
+
+  envelope.setAttributes(lcdd,x_det.regionStr(),x_det.limitsStr(),x_det.visStr());
+  return sdet;
+}
+
+DECLARE_DETELEMENT(hedra_box_envelope,create_detector)
---
+++ detector/other/tube_hedra_envelope.cpp
@@ -0,0 +1,69 @@
+//====================================================================
+//  AIDA Detector description implementation
+//--------------------------------------------------------------------
+//  DD4hep Geometry circle outer and polyhedra hole inner envelope
+//--------------------------------------------------------------------
+//  S.Lu, DESY
+//  $Id:$
+//====================================================================
+#include "DD4hep/DetFactoryHelper.h"
+#include "XML/Layering.h"
+
+using namespace std;
+using namespace DD4hep;
+using namespace DD4hep::Geometry;
+
+static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector /*sens*/)  {
+  static double tolerance = 0e0;
+  Layering      layering (e);
+  xml_det_t     x_det     = e;
+ //unused:  Material      air       = lcdd.air();
+
+  int           det_id    = x_det.id();
+  string        det_name  = x_det.nameStr();
+
+  xml_comp_t    x_dim     = x_det.dimensions();
+  int           nsides    = x_dim.numsides();
+
+  //unused: double        dphi      = (2*M_PI/nsides);
+  //unused: double        hphi      = dphi/2;
+
+  //unused: double        thickness = layering.totalThickness();
+
+  double        inner_r   = x_dim.rmin();
+  double        outer_r   = x_dim.rmax();
+
+  //assert(thickness < (outer_r - inner_r));
+  
+  Material      env_mat   = lcdd.material(x_dim.materialStr());
+
+  xml_comp_t    env_pos     = x_det.position();
+  xml_comp_t    env_rot     = x_det.rotation();
+
+  Position      pos(env_pos.x(),env_pos.y(),env_pos.z());
+  RotationZYX   rotZYX(env_rot.z(),env_rot.y(),env_rot.x());
+
+  Transform3D   tr(rotZYX,pos);
+
+  //unused:  xml_comp_t    x_staves  = x_det.staves();
+
+
+  DetElement    sdet      (det_name,det_id);
+  Volume        motherVol = lcdd.pickMotherVolume(sdet);
+
+  // The shape PolyhedraRegular for envelope. You may define your shape.
+  Tube             tube_solid(0, outer_r+tolerance*2e0,x_dim.zhalf());
+  PolyhedraRegular hedra_hole (nsides,0,inner_r+tolerance*2e0,x_dim.zhalf()*2.0+1.0e-5);
+  SubtractionSolid tubehedra(tube_solid,hedra_hole);
+  Volume        envelope  (det_name+"_envelope",tubehedra,env_mat);
+  //unused:  PlacedVolume  env_phv   = 
+  motherVol.placeVolume(envelope,tr);
+
+
+  // Then build and place the layers into the envelope in your way here.
+
+  envelope.setAttributes(lcdd,x_det.regionStr(),x_det.limitsStr(),x_det.visStr());
+  return sdet;
+}
+
+DECLARE_DETELEMENT(tube_hedra_envelope,create_detector)
