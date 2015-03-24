2429008af60128c637bdd192ab461c611357b04d
Shaojun Lu <shaojun.lu@desy.de>
2015-03-24 15:45:59 +0000

    Update ILD Ecal endcaps geometry driver to add an envelope.

--- detector/calorimeter/SEcal04_Endcaps.cpp
+++ detector/calorimeter/SEcal04_Endcaps.cpp
@@ -38,6 +38,7 @@
 #include "DD4hep/DetFactoryHelper.h"
 #include "XML/Layering.h"
 #include "DD4hep/Shapes.h"
+#include "XML/Utilities.h"
 
 using namespace std;
 using namespace DD4hep;
@@ -60,11 +61,12 @@ static Ref_t create_detector(LCDD& lcdd, xml_h element, SensitiveDetector sens) 
   DetElement    sdet      (det_name,det_id);
   Volume        motherVol = lcdd.pickMotherVolume(sdet);
 
-  Assembly envelope_assembly( det_name + "assembly"  ) ;  
-  PlacedVolume  env_phv   = motherVol.placeVolume(envelope_assembly);
+  // --- create an envelope volume and position it into the world ---------------------
 
-  env_phv.addPhysVolID("system",det_id);
-  sdet.setPlacement(env_phv);
+  Volume envelope = XML::createPlacedEnvelope( lcdd,  element , sdet ) ;
+
+  if( lcdd.buildType() == BUILD_ENVELOPE ) return sdet ;
+  //-----------------------------------------------------------------------------------
 
   sens.setType("calorimeter");
 
@@ -510,7 +512,7 @@ static Ref_t create_detector(LCDD& lcdd, xml_h element, SensitiveDetector sens) 
     Rotation3D rot3D(rot);
     Transform3D tran3D(rot3D,xyzVec);
 
-    PlacedVolume pv = envelope_assembly.placeVolume(EnvLogEndCap,tran3D);
+    PlacedVolume pv = envelope.placeVolume(EnvLogEndCap,tran3D);
     pv.addPhysVolID("module",module_id); // z: -/+ 0/6
 
     DetElement sd = (module_num==0) ? module_det : module_det.clone(_toString(module_num,"module%d"));
