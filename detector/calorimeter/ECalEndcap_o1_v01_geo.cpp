049c03db3b251f0ac5fd15812a4e7697d586dda2
Dan Protopopescu <dan.protopopescu@glasgow.ac.uk>
2015-05-29 10:32:23 +0000

    ECal EndCap driver based on the first discussions with the LLR group

--- detector/calorimeter/ECalEndcap_o1_v01_geo.cpp
+++ detector/calorimeter/ECalEndcap_o1_v01_geo.cpp
@@ -15,6 +15,7 @@
 #include "DD4hep/Printout.h"
 #include "XML/Layering.h"
 #include "TGeoTrd2.h"
+#include "TMath.h"
 #include "XML/Utilities.h"
 
 using namespace std;
@@ -35,79 +36,120 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
     if( lcdd.buildType() == BUILD_ENVELOPE ) return sdet ;
     
     //-----------------------------------------------------------------------------------
-	return sdet; //Temporary fox for the duplicated volumes -> Please fix it Dan.    
     DD4hep::XML::Component xmlParameter = x_det.child(_Unicode(parameter));
     const double tolerance = xmlParameter.attr<double>(_Unicode(ecal_endcap_tolerance));
-    const double faceThickness = xmlParameter.attr<double>(_Unicode(FaceThickness));
-    Material WDens24 = lcdd.material(xmlParameter.attr<string>(_Unicode(RadiatorMaterial)));
-    
+    const int insides = xmlParameter.attr<double>(_Unicode(InSides));
+    Material Composite = lcdd.material(xmlParameter.attr<string>(_Unicode(CarbonFiber)));
+
     Layering      layering (e);
     Material      air       = lcdd.air();
-    xml_comp_t    x_sectors = x_det.staves();
+    xml_comp_t    x_panels  = x_det.staves();
     xml_comp_t    x_dim     = x_det.dimensions();
-    int           nsides    = x_dim.numsides();
-    double        dphi      = (2.*M_PI/nsides);
+    int           outsides  = x_dim.numsides();
+    double        dphi      = (2.*M_PI/outsides);
     double        hphi      = dphi/2.;
-    double        inner_r   = x_dim.rmin()*std::sqrt(2);   // inscribing circle radius
-    double        outer_r   = x_dim.rmax()/std::cos(hphi); // circumscribing circle radius
+    double        inner_r   = x_dim.rmin();  // inscribed circle radius
+    double        outer_r   = x_dim.rmax();  // inscribed circle radius
     double        z_min     = x_dim.zmin();
     double        z_max     = x_dim.zmax();
-    double        mod_z     = layering.totalThickness();
-
-    // The endcap has a square hole for the ECal plug, but the 12/4 symmetry is awkward
-    // Will build 12 identical sectors, and each made of trapezoidal layers
-    
-    std::cout << "Sector z-thickness: " << mod_z << std::endl;
-    std::cout << "Sector r_out=" << outer_r << " and r_in=" << inner_r << std::endl;
-    std::cout << "Sector z_min=" << z_min << " and z_max=" << z_max << std::endl;
-   
-    if((z_max - z_min) < mod_z) { // throw exception
-      throw std::runtime_error("ERROR: Layers don't fit within the envelope volume!");
-    }
-
-    DetElement sector_det("sector0", det_id);
-
-    // Compute sector dimensions
-    double trd_x1 = mod_z/2.;
-    double trd_x2 = mod_z/2.;
-    double trd_y1 = inner_r*std::sin(hphi) - tolerance;
-    double trd_y2 = outer_r*std::sin(hphi) - tolerance;
-    double trd_z  = (outer_r - inner_r)*std::cos(hphi)/2 - tolerance;
-
-    // Create the trapezoid for the sector
-    Trapezoid sec(trd_x1, 
-                  trd_x2, 
-                  trd_y1, 
-                  trd_y2, 
-                  trd_z); 
-    Volume sector_vol("sector", sec, WDens24); // solid tungsten volume
+    double        mod_z     = layering.totalThickness() + 2*tolerance;
 
     sens.setType("calorimeter");
+    
+    std::cout << "Global tolerance: " << tolerance << " cm" << std::endl;
+    std::cout << "Panels z-thickness: " << mod_z << " cm" << std::endl;
+    std::cout << "EndCap z_min = " << z_min << " cm and z_max = " << z_max << " cm" << std::endl;
 
-    {// Build the sector, layer by layer and slice by slice
-      
-      // Parameters for computing the layer X dimension:
-      double l_y1 = trd_y1 - faceThickness/std::sin(hphi);  
-      double l_y2 = trd_y2 - faceThickness/std::sin(hphi);
-      double l_zh = trd_z;
-      double l_pos_z  = -trd_x1 + tolerance;
-      
+    if((z_max - z_min) < mod_z) { // throw exception
+      throw std::runtime_error("ERROR: Layers don't fit within the envelope volume!");
+    } else std::cout << "Got z_max - z_min = " << z_max - z_min << " cm" << std::endl;
+
+    // The actual panel shapes depend on whether the side of the inner cutout (l_i)
+    // is bigger or smaller than the side of the outer polygone l_o
+    double l_i = 2.*inner_r; 
+    double l_o = 2.*outer_r*std::tan(hphi);
+    std::cout << "Got l_i = " << l_i << " cm, and l_o = " << l_o << " cm" << std::endl; 
+    if(l_i < l_o) { // throw exception
+      throw std::runtime_error("ERROR: This driver is for the l_i > l_o case!");
+    } else {
+      std::cout << "EndCap r_out = " << outer_r << " cm (inscribed circle radius)" << std::endl;
+      std::cout << "EndCap r_in = " << inner_r << " cm (inscribed circle radius)" << std::endl;
+    }
+    
+    // This driver is for a 12/4 symmetry, with 4 sectors containing
+    // 3 right trapezoidal modules each (l_i > l_o)
+    // Dimensions based on the sketch from http://cern.ch/go/R89p
+    double ta = (l_i - l_o)/2;        
+    double tb = l_o*std::sin(2*dphi) - ta;
+    double tc = l_o*std::sin(dphi);
+    double td = l_o*(1 + std::cos(dphi));
+    double te = td + l_o*std::cos(2*dphi) - ta/std::tan(2*dphi);
+    double tf = tc + tb;
+    double tg = tf - ta/std::tan(2*dphi);
+
+    std::cout << "Set a=" << ta << " b=" << tb << " c=" << tc << " d=" << 
+      td << " e=" << te << " f=" << tf << " g=" << tg << " (cm)" << std::endl;
+
+    // Loop over panel modules
+    for(int m=1; m<3; m++){
+
+      DetElement panel_det("panel100", det_id);
+
+      // Function parameter names are preserved here for readability
+      // Trap(z, theta, phi, y1, x1, x2, alpha1, y2, x3, x4, alpha2)
+      // but for this y2 = y1, x3 = x1, x4 = x2, and alpha2 = alpha1
+      double z = mod_z/2;
+      double theta = 0.0;
+      double phi = M_PI/2.;
+      double y1, x1, x2, x_off, y_off;
+      double tol = 0.9999; // set at 0.96 for visulalising the panels, else ~1
+      switch(m){
+      case 1:
+	y1 = tol*tc/2; // all half-sizes
+	x1 = tol*td/2;
+	x2 = tol*l_o/2.;
+	// Offsets in definition coordinates
+        x_off = (td/2 - l_o/2)/2;
+        y_off = l_i/2 + tb + tc/2;
+	break;
+      case 2:
+	y1 = tol*tb/2;
+	x1 = tol*te/2;
+	x2 = tol*td/2;
+	x_off = (te + td)/4 - l_o/2;
+        y_off = l_i/2 + tb/2;
+	break;
+      case 3:
+      default:
+	std::cout << "With the current dimensions module 3 is too small to build" << std::endl;
+	exit(-3);
+      }   
+      double alpha1 = -TMath::ATan2(x1 - x2, 2*y1); // in radians
+      Trap trm(z, theta, phi, y1, x1, x2, alpha1, y1, x1, x2, alpha1);
+      Volume panel_vol(_toString(100*m, "panel%d"), trm, Composite);
+
+      // Build the panels, layer by layer and slice by slice
+      double l_pos_z  = -z + tolerance;
+	
       // Loop over the sets of layer elements in the detector
+      double check_thick = 0.0;
       int l_num = 1;
+      int l_set = 0;
       for(xml_coll_t li(x_det,_U(layer)); li; ++li)  {
 	
 	xml_comp_t x_layer = li;
 	int repeat = x_layer.repeat();
+
 	// Loop over number of repeats for this layer.
-	for (int j=0; j<repeat; j++)  {
+	for (int k=0; k<repeat; k++)  {
 	  
-	  string l_name = _toString(l_num, "layer%d");
-	  double l_thickness = layering.layer(j)->thickness();  // this layer's thickness
-	  
-	  Position   l_pos(l_pos_z + l_thickness/2., 0. , 0.);   // position of the layer
-	  Trapezoid  l_box(l_thickness/2., l_thickness/2., l_y1 - tolerance, l_y2 - tolerance, l_zh - tolerance);
+	  string l_name = _toString(100*m + l_num, "layer%d");
+	  double l_thickness = layering.layer(l_num-1)->thickness();  // this layer's thickness	  
+	  Position   l_pos(0, 0, l_pos_z + l_thickness/2.);           // position of the layer
+	  Trap       l_box(l_thickness/2., theta, phi, y1 - tolerance, x1 - tolerance, x2 - tolerance, alpha1, 
+			                               y1 - tolerance, x1 - tolerance, x2 - tolerance, alpha1);
 	  Volume     l_vol(l_name, l_box, air);
-	  DetElement layer(sector_det, l_name, det_id);
+	  DetElement layer(panel_det, l_name, det_id);
 	  
 	  // Loop over the slices for this layer
 	  int s_num = 1;
@@ -116,9 +158,11 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
 	    
 	    xml_comp_t x_slice = si;
 	    string     s_name  = _toString(s_num, "slice%d");
-	    double     s_thick = x_slice.thickness();
-	    //std::cout << s_name << " thickness: " << s_thick << std::endl;
-            Trapezoid  s_box(s_thick/2., s_thick/2., l_y1 - tolerance, l_y2 - tolerance, l_zh - tolerance);
+	    double     s_thick = x_slice.thickness();     // slice thickness
+	    Position   s_pos(0, 0, s_pos_z + s_thick/2);  // slice position
+	    double     xtol = tolerance + 0.00001;
+	    Trap       s_box(s_thick/2., theta, phi, y1 - xtol, x1 - xtol, x2 - xtol, alpha1, 
+                                                     y1 - xtol, x1 - xtol, x2 - xtol, alpha1);
 	    Volume     s_vol(s_name, s_box, lcdd.material(x_slice.materialStr()));
 	    DetElement slice(layer, s_name, det_id);
 	    
@@ -128,53 +172,78 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
 	    slice.setAttributes(lcdd, s_vol, x_slice.regionStr(), x_slice.limitsStr(), x_slice.visStr());
 	    
 	    // Slice placement
-	    PlacedVolume slice_phv = l_vol.placeVolume(s_vol, Position(s_pos_z + s_thick/2, 0, 0));
+	    PlacedVolume slice_phv = l_vol.placeVolume(s_vol, Position(0, 0, s_pos_z + s_thick/2));
 	    slice_phv.addPhysVolID("slice", s_num);
 	    slice.setPlacement(slice_phv);
+
+	    // Check
+	    check_thick += s_thick;
+	    /* std::cout << "Panel " << m << ", layer " <<  l_set 
+                      << ", repeat " << k << " (layer" << 100*m + l_num
+                      << "), slice " << s_num 
+		      << " (dz=" << s_thick << "cm, z=" << s_pos_z + s_thick/2 << "cm)" << std::endl;
+	    */
+
 	    // Increment Z position of slice
-	    s_pos_z += s_thick;
-	    
+	    s_pos_z += s_thick;	    
 	    // Increment slice number
 	    ++s_num;
 	  }
 	  
 	  // Set region, limitset, and visibility of layer
 	  layer.setAttributes(lcdd, l_vol, x_layer.regionStr(), x_layer.limitsStr(), x_layer.visStr());
-	  
-	  PlacedVolume layer_phv = sector_vol.placeVolume(l_vol, l_pos);
-	  layer_phv.addPhysVolID("layer", l_num);
+
+	  // std::cout << "Layer " << 100*m + l_num << std::endl;
+	  PlacedVolume layer_phv = panel_vol.placeVolume(l_vol, l_pos);
+	  layer_phv.addPhysVolID("layer", 100*m + l_num);
 	  layer.setPlacement(layer_phv);
+
 	  // Increment to next layer Z position
 	  l_pos_z += l_thickness;
+	  // Increment layer number
 	  ++l_num;
 	}
+	l_set++;
       }
-    }
 
-    // Set visualization
-    if ( x_sectors )   {
-        sector_vol.setVisAttributes(lcdd.visAttributes(x_sectors.visStr()));
-    }
+      std::cout << "Total slice thickness check " << check_thick << " cm" << std::endl;
+     
+      // Set visualization
+      if ( x_panels ) {
+      	panel_vol.setVisAttributes(lcdd.visAttributes(x_panels.visStr()));
+      }
 
-    // Phi start for sectors
-    double phi = hphi;
-    double r_off = (inner_r + outer_r)*std::cos(hphi)/2;    
-    double z_off = (z_max + z_min)/2;
+      // Placement
+      double zvis = 1.0; // for visualisation set to 1.1, else 1
+      double z_off = (z_max + zvis*z_min)/2.;
+      double r_off = std::sqrt(x_off*x_off + y_off*y_off);
+      double gamma = std::atan(x_off/y_off);
 
-    for (int j = -1; j < 2; j += 2){ 
-      for (int i = 0; i < nsides; i++, phi -= dphi) { 
-	// Compute the sector position in envelope coordinates
-	double m_pos_x = r_off * std::sin(phi);
-	double m_pos_y = r_off * std::cos(phi);
-	double m_pos_z = j*z_off;
-        Transform3D tr(RotationZYX(M_PI/2, (j+1)*M_PI/2 - j*phi, j*M_PI/2.), Translation3D(m_pos_x, m_pos_y, m_pos_z));
-	PlacedVolume pv = envelope.placeVolume(sector_vol, tr);
-	int mid = (nsides+1)*(2+j)+i;
-	pv.addPhysVolID("barrel", j); 
-	pv.addPhysVolID("sector", i);
-	DetElement sd = mid==0 ? sector_det : sector_det.clone(_toString(mid,"sector%d"));
-	sd.setPlacement(pv);
-	sdet.add(sd);
+      // Loop over endcaps
+      for (int j = -1; j < 2; j += 2){
+
+	// Loop over sectors
+	for (int i = 0; i < insides; i++) { 
+
+	  // Compute the panel position in envelope coordinates
+	  double m_pos_x = j*r_off*std::sin(gamma);
+	  double m_pos_y = r_off*std::cos(gamma);
+	  double m_pos_z = j*z_off;
+	
+	  Transform3D tr(RotationZYX(0, (j-1)*M_PI/2, 0.), Translation3D(m_pos_x, m_pos_y, m_pos_z));
+	  PlacedVolume pv = envelope.placeVolume(panel_vol, RotationZYX(i*M_PI/2. + hphi, 0., 0.)*tr);
+	  
+	  int mid = 100*(3+j)/2 + 10*i + m; // unique composed panel ID
+	  // std::cout << "Unique module ID = " << mid << std::endl;
+	  pv.addPhysVolID("barrel", j); 
+	  pv.addPhysVolID("sector", i);
+	  pv.addPhysVolID("panel", m);
+	  
+	  DetElement sd = mid==100 ? panel_det : panel_det.clone(_toString(mid,"panel%d"));
+	  sd.setPlacement(pv);
+	  sdet.add(sd);
+	  
+	}
       }
     }
 
