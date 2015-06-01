f91f5c3fdef6b889fdd8b6cc2b1f836fa88cdb5d
Marko Petric <marko.petric@cern.ch>
2015-06-01 14:45:38 +0000

    Modified drivers for Yoke and Solenoid

--- detector/calorimeter/YokeEndcap_o1_v01_geo.cpp
+++ detector/calorimeter/YokeEndcap_o1_v01_geo.cpp
@@ -1,21 +1,7 @@
-//=========================================================================
-//  Barrel ECal driver implementation for the CLIC NDM
-//-------------------------------------------------------------------------
-//  Baseon on
-//   * S. Lu's driver for SiWEcalBarrel, ported from Mokka
-//   * M. Frank's generic driver EcalBarrel_geo.cpp
-//-------------------------------------------------------------------------
-//  D.Protopopescu, Glasgow
-//  M.Frank, CERN
-//  S.Lu, DESY
-//  $Id: ECalBarrel_o1_v01_geo.cpp 328 2015-03-18 14:40:50Z /C=UK/O=eScience/OU=Glasgow/L=Compserv/CN=dan protopopescu $
-//=========================================================================
-
 #include "DD4hep/DetFactoryHelper.h"
-#include "DD4hep/Printout.h"
 #include "XML/Layering.h"
-#include "TGeoTrd2.h"
 #include "XML/Utilities.h"
+#include "DDRec/DetectorData.h"
 
 using namespace std;
 using namespace DD4hep;
@@ -36,9 +22,146 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
     if( lcdd.buildType() == BUILD_ENVELOPE ) return sdet ;
     
     //-----------------------------------------------------------------------------------
+
+    //   std::cout<<"Building Yoke EndCap inside envelope."<<std::endl;
+    xml_dim_t   dim       = x_det.dimensions();
+    Material    air       = lcdd.air();
+    int         nsides_inner = dim.nsides_inner();
+    int         nsides_outer = dim.nsides_outer();
+    double      rmin      = dim.rmin();
+    double      rmax      = dim.rmax(); /// FIXME: IS THIS RIGHT?
+    double      zmin      = dim.zmin();
+    double      phi0      = dim.phi0();
     
     
-    return sdet; //just temporary
+    Layering    layering(x_det);
+    double      totalThickness = layering.totalThickness();
+    
+    PolyhedraRegular polyVolume(nsides_outer,rmin,rmax,totalThickness);
+    
+    
+    Volume      endcapVol("endcap",polyVolume,air);
+    
+    DetElement  endcapA(sdet,"endcap",det_id);
+    Ref_t(endcapA)->SetName((det_name+"_A").c_str());
+    
+    int l_num = 1;
+    int layerType   = 0;
+    double layerZ   = -totalThickness/2;
+    
+    //Create caloData object to extend driver with data required for reconstruction
+    DDRec::LayeredCalorimeterData* caloData = new DDRec::LayeredCalorimeterData ;
+    caloData->layoutType = DDRec::LayeredCalorimeterData::EndcapLayout ;
+    caloData->inner_symmetry = nsides_inner;
+    caloData->outer_symmetry = nsides_outer;
+    caloData->phi0 = phi0;
+    /// extent of the calorimeter in the r-z-plane [ rmin, rmax, zmin, zmax ] in mm.
+    caloData->extent[0] = rmin ;
+    caloData->extent[1] = rmax ; ///FIXME: CHECK WHAT IS NEEDED (EXSCRIBED?)
+    caloData->extent[2] = zmin ;
+    caloData->extent[3] = zmin + totalThickness;
+    
+    endcapVol.setAttributes(lcdd,x_det.regionStr(),x_det.limitsStr(),x_det.visStr());
+    
+    for(xml_coll_t c(x_det,_U(layer)); c; ++c)  {
+        xml_comp_t       x_layer  = c;
+        double           l_thick  = layering.layer(l_num-1)->thickness();
+        string           l_name   = _toString(layerType,"layer%d");
+        int              l_repeat = x_layer.repeat();
+        
+        std::cout<<"Number of layers in group "<<layerType<<" : "<<l_repeat<<std::endl;
+        
+        Volume           l_vol(l_name,PolyhedraRegular(nsides_outer,rmin,rmax,l_thick),air);
+        
+        vector<PlacedVolume> sensitives;
+        
+        int s_num = 1;
+        double sliceZ = -l_thick/2;
+        double totalAbsorberThickness=0.;
+        for(xml_coll_t s(x_layer,_U(slice)); s; ++s)  {
+            xml_comp_t x_slice = s;
+            string     s_name  = _toString(s_num,"slice%d");
+            double     s_thick = x_slice.thickness();
+            Material   s_mat   = lcdd.material(x_slice.materialStr());
+            Volume     s_vol(s_name,PolyhedraRegular(nsides_outer,rmin,rmax,s_thick),s_mat);
+            
+            s_vol.setVisAttributes(lcdd.visAttributes(x_slice.visStr()));
+            sliceZ += s_thick/2;
+            PlacedVolume s_phv = l_vol.placeVolume(s_vol,Position(0,0,sliceZ));
+            s_phv.addPhysVolID("slice",s_num);
+            if ( x_slice.isSensitive() )  {
+                sens.setType("calorimeter");
+                s_vol.setSensitiveDetector(sens);
+                sensitives.push_back(s_phv);
+            }
+            
+            char val = x_slice.hasAttr(_U(radiator)) ? x_slice.attr < string > (_U(radiator))[0] : 'f';
+            val = std::toupper(val);
+            bool isAbsorber =  (val == 'T' || val == 'Y');
+            
+            if( isAbsorber ==true)
+                totalAbsorberThickness+= s_thick;
+            
+            sliceZ += s_thick/2;
+            s_num++;
+        }
+        l_vol.setVisAttributes(lcdd.visAttributes(x_layer.visStr()));
+        if ( l_repeat <= 0 ) throw std::runtime_error(x_det.nameStr()+"> Invalid repeat value");
+        for(int j=0; j<l_repeat; ++j) {
+            string phys_lay = _toString(l_num,"layer%d");
+            layerZ += l_thick/2;
+            DetElement    layer_elt(endcapA, phys_lay, l_num);
+            PlacedVolume  pv = endcapVol.placeVolume(l_vol,Position(0,0,layerZ));
+            pv.addPhysVolID("layer", l_num);
+            layer_elt.setPlacement(pv);
+            for(size_t ic=0; ic<sensitives.size(); ++ic)  {
+                PlacedVolume sens_pv = sensitives[ic];
+                DetElement comp_elt(layer_elt,sens_pv.volume().name(),l_num);
+                comp_elt.setPlacement(sens_pv);
+            }
+            
+            ///FIXME: IS ORIENTATION RIGHT? WHICH SIDE DO WE NEED TO ADD TO STRUCTURE?
+            DDRec::LayeredCalorimeterData::Layer caloLayer ;
+            caloLayer.distance = zmin + layerZ;
+            caloLayer.thickness = l_thick;
+            caloLayer.absorberThickness = totalAbsorberThickness;
+            caloLayer.cellSize0 = 30.0; //FIXME only temporary. Should get from Surfaces/Segmentation?
+            caloLayer.cellSize1 = 30.0; //FIXME
+            
+            
+            layerZ += l_thick/2;
+            ++l_num;
+        }
+        ++layerType;
+    }
+    
+    double z_pos = zmin+totalThickness/2;
+    PlacedVolume pv;
+    // Reflect it.
+    
+    DetElement  endcapB = endcapA.clone(det_name+"_B",x_det.id());
+    
+    //Removed rotations to align with envelope
+    pv = envelope.placeVolume(endcapVol,Transform3D(RotationZYX(0,0,0),
+                                                    Position(0,0,z_pos)));
+    pv.addPhysVolID("barrel", 1);
+    endcapA.setPlacement(pv);
+    
+    //Removed rotations
+    pv = envelope.placeVolume(endcapVol,Transform3D(RotationZYX(0,M_PI,0),
+                                                    Position(0,0,-z_pos)));
+    pv.addPhysVolID("barrel", 2);
+    endcapB.setPlacement(pv);
+    
+    //   pv.addPhysVolID("system", det_id);
+    //   both_endcaps.setPlacement(pv);
+    //   sdet.add(sdetA);
+    sdet.add(endcapB);
+    
+    sdet.addExtension< DDRec::LayeredCalorimeterData >( caloData ) ;
+    
+    return sdet;
+    
     
 }
 
--- detector/other/Solenoid_o1_v01_gep.cpp
+++ detector/other/Solenoid_o1_v01_gep.cpp
@@ -1,16 +1,3 @@
-//=========================================================================
-//  Barrel ECal driver implementation for the CLIC NDM
-//-------------------------------------------------------------------------
-//  Baseon on
-//   * S. Lu's driver for SiWEcalBarrel, ported from Mokka
-//   * M. Frank's generic driver EcalBarrel_geo.cpp
-//-------------------------------------------------------------------------
-//  D.Protopopescu, Glasgow
-//  M.Frank, CERN
-//  S.Lu, DESY
-//  $Id: ECalBarrel_o1_v01_geo.cpp 328 2015-03-18 14:40:50Z /C=UK/O=eScience/OU=Glasgow/L=Compserv/CN=dan protopopescu $
-//=========================================================================
-
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/Printout.h"
 #include "XML/Layering.h"
@@ -36,9 +23,56 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
     if( lcdd.buildType() == BUILD_ENVELOPE ) return sdet ;
     
     //-----------------------------------------------------------------------------------
+    Material air = lcdd.air();
+    PlacedVolume pv;
+    int n = 0;
     
+    for(xml_coll_t i(x_det,_U(layer)); i; ++i, ++n)  {
+        xml_comp_t x_layer = i;
+        string  l_name = det_name+_toString(n,"_layer%d");
+        double  z    = x_layer.outer_z();
+        double  rmin = x_layer.inner_r();
+        double  r    = rmin;
+        DetElement layer(sdet,_toString(n,"layer%d"),x_layer.id());
+        Tube    l_tub (rmin,2*rmin,z);
+        Volume  l_vol(l_name,l_tub,air);
+        int m = 0;
+        
+        for(xml_coll_t j(x_layer,_U(slice)); j; ++j, ++m)  {
+            xml_comp_t x_slice = j;
+            Material mat = lcdd.material(x_slice.materialStr());
+            string s_name= l_name+_toString(m,"_slice%d");
+            double thickness = x_slice.thickness();
+            Tube   s_tub(r,r+thickness,z,2*M_PI);
+            Volume s_vol(s_name, s_tub, mat);
+            
+            r += thickness;
+            if ( x_slice.isSensitive() ) {
+                sens.setType("tracker");
+                s_vol.setSensitiveDetector(sens);
+            }
+            // Set Attributes
+            s_vol.setAttributes(lcdd,x_slice.regionStr(),x_slice.limitsStr(),x_slice.visStr());
+            pv = l_vol.placeVolume(s_vol);
+            // Slices have no extra id. Take the ID of the layer!
+            pv.addPhysVolID("slice",m);
+        }
+        l_tub.setDimensions(rmin,r,z);
+        //cout << l_name << " " << rmin << " " << r << " " << z << endl;
+        l_vol.setVisAttributes(lcdd,x_layer.visStr());
+        
+        pv = envelope.placeVolume(l_vol);
+        pv.addPhysVolID("layer",n);
+        layer.setPlacement(pv);
+    }
+    if ( x_det.hasAttr(_U(combineHits)) ) {
+        sdet.setCombineHits(x_det.combineHits(),sens);
+    }
     
-    return sdet; //just temporary
+    //pv = lcdd.pickMotherVolume(sdet).placeVolume(envelope);
+    //pv.addPhysVolID("system",sdet.id()).addPhysVolID("barrel",0);
+    //sdet.setPlacement(pv);
+    return sdet;
     
 }
 
