7086f8b3fd3db82bbb3ab8b4cfd5a890b8a2f8d9
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-07-17 15:31:39 +0000

    BFieldMap: Fully implemented
    Example XML to add in fields section:
    
            <field name="DetectorMap" type="FieldBrBz"
                   filename="ILDMap.root"
    	       tree="fieldmap:rho:z:Brho:Bz"
    	       rScale = "1.0"
    	       zScale = "1.0"
    	       bScale = "1.0"
    	       rhoMin = "5*mm"
    	       zMin = "5*mm"
    	       rhoMax = "7000*mm"
    	       zMax = "7000*mm"
    	       nRho = "699"
    	       nZ = "699"
    	       >
            </field>

--- detector/include/FieldMapBrBz.h
+++ detector/include/FieldMapBrBz.h
@@ -3,18 +3,30 @@
 
 #include <DD4hep/FieldTypes.h>
 
-
 #include <string>
+#include <vector>
 
 class FieldMapBrBz: public DD4hep::Geometry::CartesianField::Object {
 public:
-  std::string _filename;
+
+  struct FieldValues_t {
+    double Br;
+    double Bz;
+    FieldValues_t(double _Br, double _Bz):
+      Br(_Br), Bz(_Bz) {}
+  };
+
+  int nRho, nZ;
+  double rhoMin, rhoMax, zMin, zMax, rScale, zScale, bScale;
+  std::vector< FieldValues_t > fieldMap;
 
 public:
   /// Initializing constructor
   FieldMapBrBz();
   /// Call to access the field components at a given location
   virtual void fieldComponents(const double* pos, double* field);
+  /// Fiell the FieldMap from the the tree specified in the XML
+  void fillFieldMapFromTree(const std::string& filename, const std::string& treename);
 };
 
 
--- detector/other/FieldMapBrBz.cpp
+++ detector/other/FieldMapBrBz.cpp
@@ -1,4 +1,3 @@
-
 #include "FieldMapBrBz.h"
 
 
@@ -9,27 +8,185 @@
 #include <DD4hep/DetFactoryHelper.h>
 #include <XML/Utilities.h>
 
+//Thanks stackoverflow
+#include <boost/algorithm/string.hpp>
+
 
 #include <TFile.h>
+#include <TTree.h>
 
 
 #include <string>
 #include <stdexcept>
+#include <iostream>
+#include <iomanip>
 
 DD4HEP_INSTANTIATE_HANDLE(FieldMapBrBz);
+
+namespace {
+  void checkBranch( int retCode ) {
+
+    if ( retCode != 0 ) {
+      std::stringstream error;
+      error << "FieldMap[ERROR]: Branch not correctly described ";
+      throw std::runtime_error( error.str() );
+    }
+  }
+}
 
 FieldMapBrBz::FieldMapBrBz() {
   type = DD4hep::Geometry::CartesianField::MAGNETIC;
 } //ctor
 
 
-void FieldMapBrBz::fieldComponents(const double* , double* field) {
-  field[0] = field[1] = 0.0;
-  field[2] = 300 * dd4hep::tesla;
+
+/**
+    Use bileanar interpolation to calculate the field at the given position
+    This uses large pieces from Mokka FieldX03
+ */
+void FieldMapBrBz::fieldComponents(const double* pos , double* globalField) {
+
+  //get position coordinates in our system
+  const double posRho = sqrt(pos[0]*pos[0] + pos[1]*pos[1]);
+  const double posZ   = pos[2];
+  const double posPhi = atan2(pos[1], pos[0]);
+
+  //Mokka defines these things as x and y and phi
+  double x = posRho;
+  double y = posZ;
+  double phi = posPhi;
+
+  //Get positive values to do less checks when comparing
+  if( y < 0 ) {
+    y = -y ;
+    phi += M_PI ;
+  }
+
+  //APS: Note the mokka field map does not start at 0, so we have to assume that
+  //this area is covered, or add some more parameters for the values where the
+  //field is supposed to be. Now we just assume it starts at 0/0/0, outside there is no field
+  if (not ( 0.0 <= x && x <= rhoMax &&
+	    0.0 <= y && y <= zMax ) ) {
+    globalField[0] = globalField[1] = globalField[2];
+    return;
+  }
+
+  if( x < rhoMin ) x = rhoMin;
+  if( y < zMin ) y = zMin ;
+
+
+  // Position of given point within region, normalized to the range
+  // [0,1]
+  const double xfraction = ( x - rhoMin ) / ( rhoMax - rhoMin );
+  const double yfraction = ( y - zMin )   / ( zMax   - zMin   );
+
+  // Need addresses of these to pass to modf below.
+  // modf uses its second argument as an OUTPUT argument.
+  double xdindex, ydindex;
+
+  // Position of the point within the cuboid defined by the
+  // nearest surrounding tabulated points
+  const double xlocal = ( std::modf( xfraction * ( nRho - 1), &xdindex ) );
+  const double ylocal = ( std::modf( yfraction * ( nZ   - 1), &ydindex ) );
+
+  // The indices of the nearest tabulated point whose coordinates
+  // are all less than those of the given point
+  const int xindex = static_cast<int>(xdindex);
+  const int yindex = static_cast<int>(ydindex);
+
+  const int index0 =  xindex    * nZ + yindex   ;
+  const int index1 =  xindex    * nZ + yindex+1 ;
+  const int index2 = (xindex+1) * nZ + yindex   ;
+  const int index3 = (xindex+1) * nZ + yindex+1 ;
+
+  const FieldMapBrBz::FieldValues_t& fv0 = fieldMap[index0];
+  const FieldMapBrBz::FieldValues_t& fv1 = fieldMap[index1];
+  const FieldMapBrBz::FieldValues_t& fv2 = fieldMap[index2];
+  const FieldMapBrBz::FieldValues_t& fv3 = fieldMap[index3];
+
+  double field[2] = {0.0, 0.0};
+
+  field[0] =
+    fv0.Br * (1-xlocal) * (1-ylocal)  +
+    fv1.Br * (1-xlocal) *    ylocal   +
+    fv2.Br *    xlocal  * (1-ylocal)  +
+    fv3.Br *    xlocal  *    ylocal   ;
+
+  field[1] =
+    fv0.Bz * (1-xlocal) * (1-ylocal)  +
+    fv1.Bz * (1-xlocal) *    ylocal   +
+    fv2.Bz *    xlocal  * (1-ylocal)  +
+    fv3.Bz *    xlocal  *    ylocal   ;
+
+  globalField[0] = field[0] * sin( phi ) ;
+  globalField[1] = field[0] * cos( phi ) ;
+  globalField[2] = field[1] ;
+
+
 }
 
 
-static DD4hep::Geometry::Ref_t create_FieldMap_rzBrBz(DD4hep::Geometry::LCDD& lcdd,
+void FieldMapBrBz::fillFieldMapFromTree(const std::string& filename, const std::string& treeString){
+
+  TFile *file = TFile::Open( filename.c_str() );
+  if (not file) {
+    std::stringstream error;
+    error << "FieldMap[ERROR]: File not found: " << filename;
+    throw std::runtime_error( error.str() );
+  }
+
+  std::vector<std::string> strs;
+  boost::split(strs, treeString, boost::is_any_of(": "));
+  for (std::vector<std::string>::iterator it = strs.begin(); it != strs.end(); ++it) {
+    std::cout << "\"" << (*it).c_str() << "\"" << std::endl;
+  }
+
+  if( strs.size() != 5 ) {
+    std::stringstream error;
+    error << "FieldMap[ERROR]: the treeDescription " << treeString
+	  << " is not complete. For example 'fieldmap:rho:z:Brho:Bz'";
+    throw std::runtime_error( error.str() );
+  }
+
+  TTree *tree;
+  file->GetObject(strs[0].c_str(), tree);
+  if (not tree) {
+    std::stringstream error;
+    error << "FieldMap[ERROR]: Tree " << strs[0] << " not found in file: " << filename;
+    throw std::runtime_error( error.str() );
+  }
+
+  float r, z, Br, Bz;
+  checkBranch( tree->SetBranchAddress(strs[1].c_str(), &r) );
+  checkBranch( tree->SetBranchAddress(strs[2].c_str(), &z) );
+  checkBranch( tree->SetBranchAddress(strs[3].c_str(), &Br) );
+  checkBranch( tree->SetBranchAddress(strs[4].c_str(), &Bz) );
+
+  const int elements = nRho*nZ;
+  const int treeEntries = tree->GetEntries();
+  if ( elements != treeEntries ) {
+    std::stringstream error;
+    error << "FieldMap[ERROR]: Tree does not have the same size as described in the XML "
+	  << "nRho*nZ == "  << elements
+	  << "  tree entries == " << treeEntries;
+    throw std::runtime_error( error.str() );
+  }
+
+  fieldMap.reserve(elements);
+
+  for (int i = 0; i < treeEntries ;++i) {
+    tree->GetEntry(i);
+    fieldMap.push_back( FieldMapBrBz::FieldValues_t( double(Br)*bScale*dd4hep::tesla,
+						     double(Bz)*bScale*dd4hep::tesla ) );
+
+  }
+
+  file->Close();
+  delete file;
+
+}
+
+static DD4hep::Geometry::Ref_t create_FieldMap_rzBrBz(DD4hep::Geometry::LCDD& ,
 						      DD4hep::XML::Handle_t handle ) {
   DD4hep::XML::Component xmlParameter(handle);
   bool hasFilename = xmlParameter.hasAttr(_Unicode(filename));
@@ -40,13 +197,42 @@ static DD4hep::Geometry::Ref_t create_FieldMap_rzBrBz(DD4hep::Geometry::LCDD& lc
     throw std::runtime_error(error.str());
   }
   std::string filename = xmlParameter.attr< std::string >(_Unicode(filename));
+  std::string treeString = xmlParameter.attr< std::string >(_Unicode(tree));
+
+  double rScale = xmlParameter.attr< double >(_Unicode(rScale));
+  double zScale = xmlParameter.attr< double >(_Unicode(zScale));
+  double bScale = xmlParameter.attr< double >(_Unicode(bScale));
+  double rhoMin = xmlParameter.attr< double >(_Unicode(rhoMin));
+  double rhoMax = xmlParameter.attr< double >(_Unicode(rhoMax));
+  double nRho   = xmlParameter.attr< int >   (_Unicode(nRho));
+  double zMin   = xmlParameter.attr< double >(_Unicode(zMin));
+  double zMax   = xmlParameter.attr< double >(_Unicode(zMax));
+  double nZ     = xmlParameter.attr< int >   (_Unicode(nZ));
 
   DD4hep::Geometry::CartesianField obj;
   FieldMapBrBz* ptr = new FieldMapBrBz();
-  ptr->_filename = filename;
+  ptr->rScale = rScale;
+  ptr->zScale = zScale;
+  ptr->bScale = bScale;
+  ptr->rhoMin = rhoMin*rScale;
+  ptr->rhoMax = rhoMax*rScale;
+  ptr->nRho   = nRho  ;
+  ptr->zMin   = zMin  *zScale;
+  ptr->zMax   = zMax  *zScale;
+  ptr->nZ     = nZ    ;
 
+  std::cout << "rScale  " << std::setw(13) << rScale  << std::endl;
+  std::cout << "zScale  " << std::setw(13) << zScale  << std::endl;
+  std::cout << "bScale  " << std::setw(13) << bScale  << std::endl;
+  std::cout << "rhoMin  " << std::setw(13) << rhoMin  << std::endl;
+  std::cout << "rhoMax  " << std::setw(13) << rhoMax  << std::endl;
+  std::cout << "nRho    " << std::setw(13) << nRho    << std::endl;
+  std::cout << "zMin    " << std::setw(13) << zMin    << std::endl;
+  std::cout << "zMax    " << std::setw(13) << zMax    << std::endl;
+  std::cout << "nZ      " << std::setw(13) << nZ      << std::endl;
 
   //Read the entries form the file in this place
+  ptr->fillFieldMapFromTree(filename, treeString);
 
   obj.assign(ptr, xmlParameter.nameStr(), xmlParameter.typeStr());
   return obj;
