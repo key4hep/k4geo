26c1aee0183595be95e55d3923e949332a72c5f3
Frank Gaede <frank.gaede@desy.de>
2015-07-30 14:09:14 +0000

    - switched back from TrackerCombineAction to    TrackerAction as default ( still ----- v00-05 --------)

--- example/DD4hepSimulation.py
+++ example/DD4hepSimulation.py
@@ -178,7 +178,8 @@ class DD4hepSimulation(object):
   #----------------------------------------------------------------------------------
 
     #simple = DDG4.Geant4( kernel, tracker='Geant4TrackerAction',calo='Geant4CalorimeterAction')
-    simple = DDG4.Geant4( kernel, tracker='Geant4TrackerCombineAction',calo='Geant4ScintillatorCalorimeterAction')
+    #simple = DDG4.Geant4( kernel, tracker='Geant4TrackerCombineAction',calo='Geant4ScintillatorCalorimeterAction')
+    simple = DDG4.Geant4( kernel, tracker='Geant4TrackerAction',calo='Geant4ScintillatorCalorimeterAction')
 
     simple.printDetectors()
 
