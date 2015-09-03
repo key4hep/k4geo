8ea373999c2120b74526c837f0e736c5d046c9dd
Marko Petric <marko.petric@cern.ch>
2015-09-03 08:21:39 +0000

    temp commit

--- detector/other/TrackerSupport_o1_v01.cpp
+++ detector/other/TrackerSupport_o1_v01.cpp
@@ -68,4 +68,4 @@ static Ref_t create_detector(LCDD& lcdd, xml_h e, SensitiveDetector sens)  {
     
 }
 
-DECLARE_DETELEMENT(InnerTrackerSupport_o1_v01,create_detector)
+DECLARE_DETELEMENT(TrackerSupport_o1_v01,create_detector)
