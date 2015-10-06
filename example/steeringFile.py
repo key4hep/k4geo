8d7462c4f1d23a74e1512b46b0a0352f93fb3768
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-10-06 16:26:30 +0000

    Refactor and improve features for ddsim
    * Move DD4hepSimulation et al into python module DDSim, needed for proper
      importing
    
    * Can now control magneticfield setup, gun, particle handler, output via command
      line.
    
    ** added ConfigHelper and daughter classes implementing these options
       DDSim/DDSim/Helper, easily extensible to additional features
    
    ** also easier changing of these parameters via steering file See
       example/steeringFile.py
    
    *** No more functions, no more "global GeV"
    
    ** --help string can be given by using @property, see e.g. Helper/Output.py
    
    * added optional argcomplete, allows command line "tab"complete for all command line options (and values!)
      Needs argcomplete (sudo pip install argcomplete), bash completion and bash (or zsh).
      and 'eval "$(register-python-argcomplete ddsim)"'
    
    * move import statements to reduce startup time until "--help", mandatory to
      reduce latency for argcomplete

--- example/steeringFile.py
+++ example/steeringFile.py
@@ -1,21 +1,17 @@
-from DD4hepSimulation import DD4hepSimulation
-from SystemOfUnits import GeV
-##We need to do this because DD4hepSimulation is calling execfile in a function and thus these things like GeV are only added to the local namespace of the function readSteeringFile
-global GeV
-
-
-def myGunOptions( gun ):
-  """set the starting properties of the DDG4 particle gun"""
-  print "CREATING MY OWN GUN!!!"
-  gun.energy      = 3*GeV
-  gun.particle    = "mu-"
-  gun.multiplicity = 3
-  gun.position     = (0.0,0.0,0.0)
-  gun.isotrop      = False
-  gun.direction    = (0,0,1)
-  return gun
+from DDSim.DD4hepSimulation import DD4hepSimulation
+from SystemOfUnits import mm, GeV, MeV
 
 SIM = DD4hepSimulation()
 SIM.compactFile = "/data/sailer/software/LCGeo_Auth/ILD/compact/ILD_o1_v05/ILD_o1_v05.xml"
-SIM.runType = "vis"
-SIM.setGunOptions = myGunOptions
+SIM.runType = "batch"
+SIM.numberOfEvents = 2
+
+SIM.field.eps_min = 1*mm
+
+SIM.gun.multiplicity = 1
+SIM.gun.energy = 40*GeV
+SIM.gun.direction = (1.0, 1.0, 1.0)
+
+SIM.enableGun = True
+
+SIM.part.minimalKineticEnergy = 1*MeV
