071896c11fbd308cf098d84502a0c820a845658e
Shaojun Lu <shaojun.lu@desy.de>
2016-02-03 14:01:18 +0000

    Updated 'SHcalSc04_Barrel_v01' driver, and change the ID 'slice' to 'tower' inside the driver.

--- detector/calorimeter/SHcalSc04_Barrel_v01.cpp
+++ detector/calorimeter/SHcalSc04_Barrel_v01.cpp
@@ -479,7 +479,7 @@ static Ref_t create_detector(LCDD& lcdd, xml_h element, SensitiveDetector sens) 
 	PlacedVolume slice_phv = ChamberLogical.placeVolume(slice_vol,Position(0.,0.,slice_pos_z));
 	if ( x_slice.isSensitive() ) {
 	  int slice_id  = (layer_id > Hcal_nlayers)? 1:-1;
-	  slice_phv.addPhysVolID("layer",logical_layer_id).addPhysVolID("slice",slice_id);
+	  slice_phv.addPhysVolID("layer",logical_layer_id).addPhysVolID("tower",slice_id);
 	  printout( DD4hep::DEBUG,  "SHcalSc04_Barrel_v01", "  logical_layer_id:  %d  slice_id:  %d", logical_layer_id, slice_id  ) ;
 	}
 	
