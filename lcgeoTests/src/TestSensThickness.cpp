31deea49f9ac474cc30533274314d2ea8d3eaf1b
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-12-06 15:07:00 +0000

    Add tests to check that sensitiveThickess is correctly set for the different drivers used in latest clic models
    Needs DD4hep PR AIDASoft/DD4hep#53

---
+++ lcgeoTests/src/TestSensThickness.cpp
@@ -0,0 +1,69 @@
+// Test the setting of the sensitiveThickness for the DataStructs in some Tracker detectorDrivers
+
+#include <DD4hep/DD4hepUnits.h>
+#include <DD4hep/DDTest.h>
+#include <DD4hep/Detector.h>
+#include <DD4hep/LCDD.h>
+#include <DDRec/API/IDDecoder.h>
+#include <DDRec/DetectorData.h>
+
+#include <exception>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+static DD4hep::DDTest test( "SensThickness" ) ;
+
+template<typename T> void checkExtensions( DD4hep::Geometry::LCDD& lcdd, std::vector<std::string> detectorNames, std::vector<double> sensThickness) {
+
+  int counter = 0;
+  for (auto const& detName : detectorNames ) {
+    const double thickness = sensThickness[counter]; ++counter;
+    auto detElement = lcdd.detector(detName);
+    auto* theExtension = detElement.extension<T>();
+    const std::vector<typename T::LayerLayout>& layers= theExtension->layers;
+    int layerCount = 0;
+    for (auto const& layer : layers) {
+      std::stringstream msg;
+      msg << detName << " layer " << layerCount << ": "
+	  << "layer sensitive thickness:  " << layer.thicknessSensitive/dd4hep::micrometer
+	  << "  expected " << thickness/dd4hep::micrometer;
+      test( fabs(layer.thicknessSensitive - thickness)  < 1e-11, msg.str() );
+      ++layerCount;
+    }
+  }
+}
+
+
+int main (int argc, char **args) {
+
+  if ( argc != 4 ){
+    throw std::runtime_error( "need to provide compact file and thickness for tracker and vertex silicon thicknesses in micrometer");
+  }
+  std::string compactFile = std::string(args[1]);
+
+  const double trackerThick = atof(args[2]);
+  const double vertexThick = atof(args[3]);
+
+  DD4hep::Geometry::LCDD& lcdd = DD4hep::Geometry::LCDD::getInstance();
+  lcdd.fromCompact( compactFile );
+
+  std::vector<std::string> endcaps = { "InnerTrackerEndcap",
+				       "OuterTrackerEndcap",
+				       "VertexEndcap"};
+
+  std::vector<std::string> barrels = {"InnerTrackerBarrel",
+				      "OuterTrackerBarrel",
+				      "VertexBarrel" };
+
+  std::vector<double> sensThickness = { trackerThick*dd4hep::micrometer,
+					trackerThick*dd4hep::micrometer,
+					vertexThick*dd4hep::micrometer };
+
+  checkExtensions<DD4hep::DDRec::ZDiskPetalsData>( lcdd, endcaps, sensThickness );
+  checkExtensions<DD4hep::DDRec::ZPlanarData>( lcdd, barrels, sensThickness );
+
+
+}
