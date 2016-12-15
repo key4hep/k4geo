52886fb25b9021ec3fdf664cb961cf3fa929c8a1
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-12-15 13:40:36 +0100

    Add Travis Configuration

---
+++ .travis-ci.d/compile_and_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
+source $ILCSOFT/init_ilcsoft.sh
+
+cd /Package
+mkdir build
+cd build
+cmake -C$ILCSOFT/ILCSoft.cmake -GNinja -DUSE_CXX11=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+    ninja && \
+    ninja install && \
+    ctest --output-on-failure
