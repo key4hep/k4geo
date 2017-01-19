00fb02bcaf8497f9c150ff3846fa60f78e07299b
Luis Alejandro Perez Perez <laperez@nafhh-ilc02.desy.de>
2017-01-19 14:41:29 +0100

    Adding new field-map XYZ

---
+++ detector/include/FieldMapXYZ.h
@@ -0,0 +1,33 @@
+#ifndef FieldMap_XYZ_h
+#define FieldMap_XYZ_h 1
+
+#include <DD4hep/FieldTypes.h>
+
+#include <string>
+#include <vector>
+
+class FieldMapXYZ: public DD4hep::Geometry::CartesianField::Object {
+public:
+
+  struct FieldValues_t {
+    double Br;
+    double Bz;
+    FieldValues_t(double _Br, double _Bz):
+      Br(_Br), Bz(_Bz) {}
+  };
+
+  int nRho, nZ;
+  double rhoMin, rhoMax, zMin, zMax, rScale, zScale, bScale;
+  std::vector< FieldValues_t > fieldMap;
+
+public:
+  /// Initializing constructor
+  FieldMapXYZ();
+  /// Call to access the field components at a given location
+  virtual void fieldComponents(const double* pos, double* field);
+  /// Fiell the FieldMap from the the tree specified in the XML
+  void fillFieldMapFromTree(const std::string& filename, const std::string& treename);
+};
+
+
+#endif // FieldMap_rzBrBz_h
