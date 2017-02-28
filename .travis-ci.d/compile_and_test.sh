2031daf900ee187caa8708c90340b5ece13611cf
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-02-28 11:43:52 +0100

    Keep compilation going after error

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -7,6 +7,6 @@ cd /Package
 mkdir build
 cd build
 cmake -C$ILCSOFT/ILCSoft.cmake -GNinja -DUSE_CXX11=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
-    ninja && \
+    ninja -k 0 && \
     ninja install && \
     ctest --output-on-failure
