87c8b1de59623cdf1b88704ee4983170b17eb8f5
luisaleperez <luis_alejanro.perez_perez@iphc.cnrs.fr>
2017-05-23 19:27:54 +0200

    Field maps can be now specified in any order of variables, and the individual variables can be specified from low-to-high or high-to-low

--- detector/include/FieldMapBrBz.h
+++ detector/include/FieldMapBrBz.h
@@ -16,28 +16,32 @@ public:
       Br(_Br), Bz(_Bz) {}
   };
 
-  int CoorsOrder;
-  std::string  StrCoorsOrder;
-  std::string  NtupleName;
-  std::string  rhoVar;
-  std::string  zVar;
-  std::string  BrhoVar;
-  std::string  BzVar;
-  int nRho, nZ;
-  double rhoMin, rhoMax, rhoStep, rScale;
-  double zMin,   zMax,   zStep,   zScale;
-  double bScale;
-  std::vector< FieldValues_t > fieldMap;
+  int CoorsOrder;             // integer with the order with which variables are scanned in the fieldmap, 1(2) for RZ(ZR) order
+  std::string  StrCoorsOrder; // string  with the order with which variables are scanned in the fieldmap, RZ or ZR order
+  std::string  NtupleName;    // tree name
+  std::string  rhoVar;        // rho  coordinate name in tree
+  std::string  zVar;          // z    coordinate name in tree
+  std::string  BrhoVar;       // Brho component  name in tree
+  std::string  BzVar;         // Bz   component  name in tree
+
+  int nRho, nZ;                            // bins in rho and z coordinates in fieldmap
+  int rhoOrdering;                         // rho coordinate ordering, 1(-1) if from low-to-high (high-to-low)
+  double rhoMin, rhoMax, rhoStep, rScale;  // min, max, step-size and scale factor of rho coordinate in fieldmap
+  int zOrdering;                           // z   coordinate ordering, 1(-1) if from low-to-high (high-to-low)
+  double zMin,   zMax,   zStep,   zScale;  // min, max, step-size and scale factor of z   coordinate in fieldmap
+
+  double bScale;                           //Bfield scale factor
+  std::vector< FieldValues_t > fieldMap;   //List with the field map points
 
 public:
   /// Initializing constructor
   FieldMapBrBz();
   /// Call to access the field components at a given location
   virtual void fieldComponents(const double* pos, double* field);
-  /// Fill the FieldMap from the the tree specified in the XML
+  /// Field the FieldMap from the the tree specified in the XML
   void fillFieldMapFromTree(const std::string& filename, double coorUnits, double BfieldUnits);
   /// Get global index in the Field map
-  int getGlobalIndex(const double r, const double z);
+  int getGlobalIndex(const int rBin, const int zBin);
 };
 
 
--- detector/include/FieldMapXYZ.h
+++ detector/include/FieldMapXYZ.h
@@ -17,23 +17,26 @@ public:
       Bx(_Bx), By(_By), Bz(_Bz) {}
   };
 
-  int CoorsOrder;
-  std::string  StrCoorsOrder;
-  std::string  NtupleName;
-  std::string  xVar;
-  std::string  yVar;
-  std::string  zVar;
-  std::string  BxVar;
-  std::string  ByVar;
-  std::string  BzVar;
-  int nX, nY, nZ; 
-  double xMin,xMax,xStep,xScale;
-  double yMin,yMax,yStep,yScale;
-  double zMin,zMax,zStep,zScale;
+  int CoorsOrder;             // integer with the order with which variables are scanned in the fieldmap, 1(2) for RZ(ZR) order
+  std::string  StrCoorsOrder; // string  with the order with which variables are scanned in the fieldmap, RZ or ZR order
+  std::string  NtupleName;    // tree name
+  std::string  xVar;          // x  coordinate name in tree
+  std::string  yVar;          // y  coordinate name in tree
+  std::string  zVar;          // z  coordinate name in tree
+  std::string  BxVar;         // Bx component  name in tree
+  std::string  ByVar;         // By component  name in tree
+  std::string  BzVar;         // Bz component  name in tree
+
+  int nX, nY, nZ;                // bins in x, y and z coordinates in fieldmap
+  int xOrdering;                 // x coordinate ordering, 1(-1) if from low-to-high (high-to-low)
+  double xMin,xMax,xStep,xScale; // min, max, step-size and scale factor of x coordinate in fieldmap
+  int yOrdering;                 // y coordinate ordering, 1(-1) if from low-to-high (high-to-low)
+  double yMin,yMax,yStep,yScale; // min, max, step-size and scale factor of y coordinate in fieldmap
+  int zOrdering;                 // z coordinate ordering, 1(-1) if from low-to-high (high-to-low)
+  double zMin,zMax,zStep,zScale; // min, max, step-size and scale factor of z coordinate in fieldmap
   
-  double bScale;
-  
-  std::vector< FieldValues_t > fieldMap;
+  double bScale;                         //Bfield scale factor 
+  std::vector< FieldValues_t > fieldMap; //List with the field map points
 
 public:
   /// Initializing constructor
@@ -41,11 +44,10 @@ public:
   
   /// Call to access the field components at a given location
   virtual void fieldComponents(const double* pos, double* field);
-  
-  /// Fiell the FieldMap from the the tree specified in the XML
+  /// Field the FieldMap from the the tree specified in the XML
   void fillFieldMapFromTree(const std::string& filename, double coorUnits, double BfieldUnits);
-  
-  int   getGlobalIndex(double x, double y, double z);
+  /// Get global index in the Field map 
+  int  getGlobalIndex(const int xBin, const int yBin, const int zBin);
   
 };
 
