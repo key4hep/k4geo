ad69d11186fa9b93771fb42a4b956ab8fe7f9911
Armin Fehr <armin.fehr@lhep.unibe.ch>
2023-05-26 07:59:09 +0200

    Adding IDEA as FCCee_IDEA_o1_v01, not final yet, need to make sure that latest beam pipe and other files are used. There's also still an overlap between the CAD-imported vertex support and the vertex barrel

---
+++ FCCee/compact/FCCee_IDEA_o1_v01/scripts/teveDisplay.sh
@@ -0,0 +1,8 @@
+### Script to look at detector geometry (warning: very slow for complex detectors)
+### and over bad connection
+
+# IDEA Vertex
+# teveDisplay -ui -compact FCCee_IDEA_o1_v01.xml 
+
+# IDEA Vertex IDEA
+teveDisplay -ui -compact FCCee_IDEA_Empty_o1_v01.xml Vertex_IDEA_o1_v01.xml 
