a7b9a8627da626f9f3be163459d8d1833822326d
Armin Fehr <armin.fehr@lhep.unibe.ch>
2023-06-22 08:51:47 +0200

    Addressing comments from MR, adding some tests for DDCAD files

--- FCCee/compact/FCCee_IDEA_o1_v01/scripts/check_material_budget.sh
+++
@@ -1,2 +0,0 @@
-k4run scripts/utils/material_scan.py 
-python scripts/utils/material_plots.py --fname out_material_scan.root --etaMax 3 --x0max 0.18 # eta of 3 is equal to theta of 0.099, so 99 mrad
--- FCCee/compact/FCCee_IDEA_o1_v01/scripts/overlap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-### Script to check for overlaps, as described in 
-### https://hep-fcc.github.io/fcc-tutorials/full-detector-simulations/Geometry/Geometry.html#overlap-checking
-
-# IDEA
-nohup ddsim --compactFile FCCee_IDEA_o1_v01.xml --runType run --macroFile scripts/utils/overlap.mac &> overlap.log &
