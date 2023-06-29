f4ee3916956dcb25c1d8e07ad8c377a7c3802319
Armin Fehr <armin.fehr@lhep.unibe.ch>
2023-06-29 15:15:29 +0200

    Changing naming convention to Vertex inner barrel, vertex outer barrel and vertex disks, adding working DDCAD import of vertex inner barrel support (using material budget estimation from FCCSW not working yet though, need to use g4MaterialScan). Adding example on how I install k4geo locally (install.sh)

---
+++ FCCee/compact/IDEA/IDEA_o1_v01/install.sh
@@ -0,0 +1,11 @@
+cd ../../../../
+mkdir build
+cd build
+source $DD4hep_DIR/bin/thisdd4hep.sh
+
+cmake .. -DCMAKE_INSTALL_PREFIX=../InstallArea -DBoost_NO_BOOST_CMAKE=ON
+make -j4 install
+cd ../InstallArea
+source bin/thislcgeo.sh
+
+export PYTHONPATH=${LCIO}/src/python:${ROOTSYS}/lib:$PYTHONPATH
