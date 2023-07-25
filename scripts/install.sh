d37be7e95bccf8e67ed56448e4ae0fee6b16db94
armin-ilg <armin.ilg@cern.ch>
2023-07-25 13:24:08 +0200

    Found another way to get the correct paths for the scripts, using /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-07-22/x86_64-centos7-gcc12.2.0-opt/k4geo/1c08affe597cf3b1c10c4e81cdbc95ab9e4a4d23=develop-q7e4mj/share/k4geo/compact for the DDCAD import in Vertex_IDEA_o1_v01.xml, should work once merged and added to the key4hep nightly

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,10 @@
 # using the nightly release
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh                                                                                                                                                                                                                                                                                                              
 
-cd ..
+script_folder="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # Get the location of this script, to execute correctly other commands
+cd $script_folder/..
+
+echo "Running on xml file $xml"
 mkdir build
 cd build
 source $DD4hep_DIR/bin/thisdd4hep.sh
--- scripts/overlap.sh
+++ scripts/overlap.sh
@@ -3,9 +3,13 @@
 ### Script to check for overlaps, as described in 
 ### https://hep-fcc.github.io/fcc-tutorials/full-detector-simulations/Geometry/Geometry.html#overlap-checking
 
-xml=$1
-    if [[ -n "$xml" ]]; then # test to see if not empty
-    nohup ddsim --compactFile ${xml} --runType run --macroFile ${k4geo_DIR}/utils/overlap.mac &> overlap.log &
+script_folder="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # Get the location of this script, to execute correctly other commands
+xml=$1 # Give xml detector file as input argument!
+
+echo "Running on xml file $xml"
+
+if [[ -n "$xml" ]]; then # test to see if not empty
+    nohup ddsim --compactFile ${xml} --runType run --macroFile ${script_folder}/../utils/overlap.mac &> overlap.log &
 else
     echo "argument error, please provide an xml file as input argument!"
 fi
