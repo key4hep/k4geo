5514d0374b95bd4212242085b13a87371d55a204
armin-ilg <armin.ilg@cern.ch>
2023-07-25 13:24:08 +0200

    Found another way to get the correct paths for the scripts, using /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-07-22/x86_64-centos7-gcc12.2.0-opt/k4geo/1c08affe597cf3b1c10c4e81cdbc95ab9e4a4d23=develop-q7e4mj/share/k4geo/compact for the DDCAD import in Vertex_IDEA_o1_v01.xml, should work once merged and added to the key4hep nightly

--- scripts/build_view.sh
+++ scripts/build_view.sh
@@ -2,12 +2,15 @@
 ### that can be easily viewed using e.g https://root.cern/js/latest/
 ### as described in https://hep-fcc.github.io/fcc-tutorials/full-detector-simulations/Visualization/Visualization.html#detector-geometry
 
-chmod u+x ${k4geo_DIR}/utils/dd4hep2root
 
-xml=$1
+script_folder="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # Get the location of this script, to execute correctly other commands
+xml=$1 # Give xml detector file as input argument!
+
+echo "Running on xml file $xml"
 
 if [[ -n "$xml" ]]; then # test to see if not empty
-    ${k4geo_DIR}/utils/dd4hep2root -c ${xml} -o detector_dd4hep2root.root
+    chmod u+x ${script_folder}/../utils/dd4hep2root
+    ${script_folder}/../utils/dd4hep2root -c ${xml} -o detector_dd4hep2root.root
 else
     echo "argument error, please provide an xml file as input argument!"
 fi
