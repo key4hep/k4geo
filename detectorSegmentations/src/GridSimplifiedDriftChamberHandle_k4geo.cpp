3afce23368ea43f1c9c0246fcfdb11d44b4161a9
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-09-05 15:44:20 +0200

    GridSimplifiedDriftChamberHandle refactor

--- detectorSegmentations/src/GridSimplifiedDriftChamberHandle_k4geo.cpp
+++ detectorSegmentations/src/GridSimplifiedDriftChamberHandle_k4geo.cpp
@@ -1,4 +1,4 @@
-#include "detectorSegmentations/GridSimplifiedDriftChamberHandle.h"
+#include "detectorSegmentations/GridSimplifiedDriftChamberHandle_k4geo.h"
 #include "DD4hep/detail/Handle.inl"
 
 DD4HEP_INSTANTIATE_HANDLE_UNNAMED(dd4hep::DDSegmentation::GridSimplifiedDriftChamber);
