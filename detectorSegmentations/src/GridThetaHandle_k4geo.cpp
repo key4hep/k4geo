06e575391cd0a38020ff7d3b65a7d0aa34a902d8
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-09-05 15:39:27 +0200

    GridThetaHandle refactor

--- detectorSegmentations/src/GridThetaHandle_k4geo.cpp
+++ detectorSegmentations/src/GridThetaHandle_k4geo.cpp
@@ -1,4 +1,4 @@
-#include "detectorSegmentations/GridThetaHandle.h"
+#include "detectorSegmentations/GridThetaHandle_k4geo.h"
 #include "DD4hep/detail/Handle.inl"
 
 DD4HEP_INSTANTIATE_HANDLE_UNNAMED(dd4hep::DDSegmentation::GridTheta_k4geo);
