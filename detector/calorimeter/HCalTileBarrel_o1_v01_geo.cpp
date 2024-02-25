cf2c07ef49225ccb39827c038cc0b3c11f12bc0b
jmcarcell <jmcarcell@users.noreply.github.com>
2024-02-25 19:25:10 +0100

    Remove unused string

--- detector/calorimeter/HCalTileBarrel_o1_v01_geo.cpp
+++ detector/calorimeter/HCalTileBarrel_o1_v01_geo.cpp
@@ -12,7 +12,6 @@ using dd4hep::PlacedVolume;
 #define endmsg std::endl
 #define lLog std::cout
 namespace MSG {
-const std::string ERROR = " Error: ";
 const std::string DEBUG = " Debug: ";
 const std::string INFO  = " Info: ";
 }
