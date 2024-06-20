e2fee6335cb4324bcb65526d416e2cdd50491d20
Erich Varnes <evarnes@atlng01.physics.arizona.edu>
2024-06-19 20:24:32 -0700

    updates in preparation for pull request

--- detectorSegmentations/src/FCCSWEndcapTurbine_k4geo.cpp
+++ detectorSegmentations/src/FCCSWEndcapTurbine_k4geo.cpp
@@ -10,15 +10,10 @@ namespace DDSegmentation {
   _type = "FCCSWEndcapTurbine_k4geo";
   _description = "Turbine-specific segmentation in the global coordinates";
 
-  // register all necessary parameters (additional to those registered in GridTheta_k4geo)
-  //  registerParameter("phi_bins", "Number of bins phi", m_phiBins, 1);
-  //registerParameter("offset_phi", "Angular offset in phi", m_offsetPhi, 0., SegmentationParameter::AngleUnit, true);
-  //registerIdentifier("identifier_phi", "Cell ID identifier for phi", m_phiID, "phi");
-  //registerParameter("rho_bins", "Number of bins in rho", m_rhoBins, 1);
-  registerParameter("grid_size_rho", "Grid size in rho", m_gridSizeRho, 0.);
-  registerParameter("offset_rho", "Offset in rho", m_offsetRho, 0.);
-  registerIdentifier("identifier_rho", "Cell ID identifier for rho", m_rhoID, "rho");
-  //registerParameter("z_bins", "Number of bins in z", m_zBins, 1);
+  // register all necessary parameters
+  registerParameter("grid_size_rho", "Grid size in rho", m_gridSizeRho, std::vector<float>());
+  registerParameter("offset_rho", "Offset in rho", m_offsetRho, std::vector<float>());
+  registerIdentifier("identifier_rho", "Cell ID identifier for rho", m_rhoID, "rho"); // might want to have separate concepts for rho and layer...
   registerParameter("grid_size_z", "Grid size in z", m_gridSizeZ, 0.);
   registerParameter("offset_z", "Offset in z", m_offsetZ, 0.);
   registerIdentifier("identifier_z", "Cell ID identifier for z", m_zID, "z");
@@ -41,19 +36,16 @@ namespace DDSegmentation {
   _type = "FCCSWEndcapTurbine_k4geo";
   _description = "Turbine-specific segmentation in the global coordinates";
 
-  // register all necessary parameters (additional to those registered in GridTheta_k4geo)
-  //  registerParameter("phi_bins", "Number of bins phi", m_phiBins, 1);
-  // registerParameter("offset_phi", "Angular offset in phi", m_offsetPhi, 0., SegmentationParameter::AngleUnit, true);
-  // registerIdentifier("identifier_phi", "Cell ID identifier for phi", m_phiID, "phi");
-  //  registerParameter("rho_bins", "Number of bins in rho", m_rhoBins, 1);
-  registerParameter("grid_size_rho", "Grid size in rho", m_gridSizeRho, 0.);
-  registerParameter("offset_rho", "Offset in rho", m_offsetRho, 0.);
+  // register all necessary parameters
+  registerParameter("grid_size_rho", "Grid size in rho", m_gridSizeRho, std::vector<float>());
+  registerParameter("offset_rho", "Offset in rho", m_offsetRho, std::vector<float>());
   registerIdentifier("identifier_rho", "Cell ID identifier for rho", m_rhoID, "rho");
-  //  registerParameter("z_bins", "Number of bins in z", m_zBins, 1);
   registerParameter("grid_size_z", "Grid size in z", m_gridSizeZ, 0.);
   registerParameter("offset_z", "Offset in z", m_offsetZ, 0.);
   registerIdentifier("identifier_z", "Cell ID identifier for z", m_zID, "z");
   registerIdentifier("identifier_side", "Cell ID identifier for side", m_sideID, "side");
+  registerIdentifier("identifier_wheel", "Cell ID identifier for wheel", m_wheelID, "wheel");
+  registerIdentifier("identifier_module", "Cell ID identifier for module", m_moduleID, "module");
   dd4hep::Detector* dd4hepgeo = &(dd4hep::Detector::getInstance());
   try {
     m_bladeAngle = dd4hepgeo->constant<double>("BladeAngle");
@@ -69,15 +61,12 @@ namespace DDSegmentation {
 
 /// determine the local position based on the cell ID
 Vector3D FCCSWEndcapTurbine_k4geo::position(const CellID& cID) const {
-  //  Vector3D localPos = PositionRhoZPhi(rho(cID), z(cID), phi(cID));
-  //  std::cout << "Local position is " << localPos.x() << " " << localPos.y() << " " << localPos.z() << std::endl;
-  //Vector3D dummyPos(z(cID),0,rho(cID));
-  //Vector3D dummyPos(x(cID),0,z(cID));  /* this one gets z right, but not x and y */
-  //Vector3D dummyPos(x(cID),rho(cID),z(cID)); /* screws up z, seems to result in anti-correlation and offset in x, y looks random */
-  Vector3D dummyPos(0, 0, 0);
-  return dummyPos;
-  // return PositionRhoZPhi(rho(cID), z(cID), phi(cID));
-  // return PositionRhoZPhi(0, rho(cID), z(cID)/rho(cID));
+
+  double rhoVal = rho(cID);
+  double zVal = z(cID);
+  double phiVal = phi(cID);
+  std::cout << "returning rho, z, phi = " << rhoVal << " " << zVal << " " << phiVal << std::endl;
+  return PositionRhoZPhi(rhoVal, zVal, phiVal);
 }
 
 /// determine the cell ID based on the position
@@ -85,77 +74,65 @@ CellID FCCSWEndcapTurbine_k4geo::cellID(const Vector3D& /* localPosition */, con
                           const VolumeID& vID) const {
   CellID cID = vID;
   double lRho = rhoFromXYZ(globalPosition);
-  _decoder->set(cID, m_rhoID, positionToBin(lRho, m_gridSizeRho, m_offsetRho));
-  // std::cout << "vID = " << std::hex <<vID << std::dec << std::endl;
-  //std::cout << "lRho = " << lRho << std::endl;
-  //std::cout << "bin = " << positionToBin(lRho, m_gridSizeRho, m_offsetRho) << std::endl;
+  CellID iWheel = _decoder->get(cID, m_wheelID);
+  std::cout << "iWheel = " << iWheel << std::endl;
+  _decoder->set(cID, m_rhoID, positionToBin(lRho, m_gridSizeRho[iWheel], m_offsetRho[iWheel]));
+  std::cout << "lRho = " << lRho << std::endl;
+  std::cout << "bin = " << positionToBin(lRho, m_gridSizeRho[iWheel], m_offsetRho[iWheel]) << std::endl;
 
   double lZ = TMath::Abs(globalPosition.Z);
-  //std::cout << "Actual Z position is " << lZ << std::endl;
-  //std::cout << "Thus Z bin is " << positionToBin(lZ, m_gridSizeZ, m_offsetZ) << std::endl;
+  std::cout << "Actual Z position is " << lZ << std::endl;
+  std::cout << "Thus Z bin is " << positionToBin(lZ, m_gridSizeZ, m_offsetZ) << std::endl;
   _decoder->set(cID, m_zID, positionToBin(lZ, m_gridSizeZ, m_offsetZ));
 
-  //std::cout << "cID = " << std::hex << cID << std::dec << std::endl;  
-  //std::cout << "Global position (x,y,z) and cellID: " << globalPosition.X << " "  << globalPosition.Y << " " << globalPosition.Z << " " << cID << std::endl;
-
-  //  double lTheta = thetaFromXYZ(globalPosition);
-  // double lPhi = phiFromXYZ(globalPosition);
-  //_decoder->set(cID, m_thetaID, positionToBin(lTheta, m_gridSizeTheta, m_offsetTheta));
-  //_decoder->set(cID, m_phiID, positionToBin(lPhi, 2 * M_PI / (double)m_phiBins, m_offsetPhi));
   return cID;
 }
 
-/// determine the azimuthal angle phi based on the current cell ID
-//double FCCSWEndcapTurbine_k4geo::phi() const {
-//  CellID phiValue = (*_decoder)[m_phiID].value();
-//  return binToPosition(phiValue, 2. * M_PI / (double)m_phiBins, m_offsetPhi);
-//}
-
 /// determine the azimuthal angle phi based on the cell ID
 double FCCSWEndcapTurbine_k4geo::phi(const CellID& cID) const {
-  // CellID phiValue = _decoder->get(cID, m_phiID);
-  //return binToPosition(phiValue, 2. * M_PI / (double)m_phiBins, m_offsetPhi);
-  double rhoLoc = rho(cID)-m_offsetRho;
-  double zLoc = TMath::Abs(x(cID))-m_offsetZ;
-  double zCotBladeAngle = zLoc/TMath::Tan(m_bladeAngle);
 
-  //  std::cout << "Calculated phi is " << TMath::ATan(zCotBladeAngle/TMath::Sqrt(rhoLoc*rhoLoc-zCotBladeAngle*zCotBladeAngle)) << std::endl;
-    
-  //  return 0;
+  CellID iModule = _decoder->get(cID, m_moduleID);
+  double phiCent = twopi*iModule/78336;
 
-  return TMath::ATan(zCotBladeAngle/TMath::Sqrt(rhoLoc*rhoLoc-zCotBladeAngle*zCotBladeAngle));
+  CellID iWheel = _decoder->get(cID, m_wheelID);
+  double rhoLoc = rho(cID);
+  double zLoc = TMath::Abs(z(cID))-m_offsetZ - 45;  // hard-code midpoint in z for now
+
+  CellID sideValue = _decoder->get(cID, m_sideID);
+  double zCotBladeAngle = ((long long int)sideValue)*zLoc/TMath::Tan(m_bladeAngle);
+  double x = zCotBladeAngle;
+  double y = TMath::Sqrt(rhoLoc*rhoLoc - x*x);
+  // rotate about z axis by phiCent
+  double xprime = x*TMath::Cos(phiCent) +y*TMath::Sin(phiCent);
+  double yprime = y*TMath::Cos(phiCent) -x*TMath::Sin(phiCent);
+  
+  return TMath::ATan2(xprime,yprime);
 }
 /// determine the transverse distance from the beamline r based on the cell ID
 double FCCSWEndcapTurbine_k4geo::rho(const CellID& cID) const {
-  //std::cout << "Calculating rho... " << std::endl;
+  std::cout << "Calculating rho... " << std::endl;
   CellID rhoValue = _decoder->get(cID, m_rhoID);
-  //std::cout << "Calculated bin is " << rhoValue << std::endl;
-  //std::cout << "Thus, calculated rho position is " << binToPosition(rhoValue,m_gridSizeRho) << std::endl;
-  return binToPosition(rhoValue,m_gridSizeRho);
+  CellID iWheel = _decoder->get(cID, m_wheelID);
+
+  return binToPosition(rhoValue,m_gridSizeRho[iWheel], m_offsetRho[iWheel]);
 }
 /// determine local x in plane of blade based on the cell ID
-double FCCSWEndcapTurbine_k4geo::x(const CellID& cID) const {
-  //std::cout << "Calculating x for cID " << std::hex << cID << std::dec <<std::endl;
+double FCCSWEndcapTurbine_k4geo::z(const CellID& cID) const {
   CellID zValue = _decoder->get(cID, m_zID);
   CellID sideValue = _decoder->get(cID, m_sideID);
-  //std::cout << "Calculated bin is " << (long long int)zValue << std::endl;
-  //std::cout << "Side value is " << (long long int)sideValue << std::endl;
-  //std::cout << "Thus, calculated x position is " << binToPosition(zValue,m_gridSizeZ)/TMath::Cos(m_bladeAngle) << std::endl;
-  return -((long long int)sideValue)*binToPosition(zValue,m_gridSizeZ)/TMath::Cos(m_bladeAngle);
+  return ((long long int)sideValue)*binToPosition(zValue,m_gridSizeZ,m_offsetZ);
 }
 
 /// determine local y in plane of blade based on cellID
-double FCCSWEndcapTurbine_k4geo::z(const CellID& cID) const {
+double FCCSWEndcapTurbine_k4geo::y(const CellID& cID) const {
 
-  //std::cout << "Calculating z for cID " << std::hex << cID << std::dec <<std::endl;
   CellID zValue = _decoder->get(cID, m_zID);
   CellID rhoValue = _decoder->get(cID, m_rhoID);
-
+  CellID iWheel = _decoder->get(cID, m_wheelID);
   double zGlob=  binToPosition(zValue,m_gridSizeZ);
-  double rho= binToPosition(rhoValue,m_gridSizeRho)+m_offsetRho;
-  //std::cout << "Rho, zGlob are " << rho << ", " << zGlob << std::endl; 
-  //std::cout << "Calculated z position is " << -TMath::Sqrt(rho*rho-zGlob*zGlob) - m_offsetRho << std::endl;
-  return -TMath::Sqrt(rho*rho-zGlob*zGlob) - m_offsetRho;
+  double rho= binToPosition(rhoValue,m_gridSizeRho[iWheel])+m_offsetRho[iWheel];
+
+  return -TMath::Sqrt(rho*rho-zGlob*zGlob) - m_offsetRho[iWheel];
 
 }
 }
