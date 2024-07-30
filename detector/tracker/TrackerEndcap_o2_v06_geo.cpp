5a9e5de8a9ee17bba5e71c8a15562673cc1a97cf
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2024-07-30 14:04:54 +0200

    fix CLD endcap radius calculation for CED

--- detector/tracker/TrackerEndcap_o2_v06_geo.cpp
+++ detector/tracker/TrackerEndcap_o2_v06_geo.cpp
@@ -184,11 +184,11 @@ static Ref_t create_detector(Detector& theDetector, xml_h e, SensitiveDetector s
             
             Box mod_shape(m_vol.solid());
             
-             if(r - mod_shape->GetDZ()<innerR)
-                 innerR= r- mod_shape->GetDZ();
+             if(r < innerR)
+                 innerR = r;
              
-             if(r + mod_shape->GetDZ()>outerR)
-                 outerR = r + mod_shape->GetDZ();
+             if(r + 2*mod_shape->GetDY() > outerR)
+                 outerR = r + 2*mod_shape->GetDY();
                  
             sumZ+=zstart;
             
