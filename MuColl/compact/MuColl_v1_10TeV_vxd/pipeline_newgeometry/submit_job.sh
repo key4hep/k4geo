dcdcb7ef21e43721e0fad57c7ef42b5699774120
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2024-10-10 09:43:46 +0200

    Removed old geometries for 10TeV

--- MuColl/compact/MuColl_v1_10TeV_vxd/pipeline_newgeometry/submit_job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-Marlin --global.LCIOInputFiles="muonGun_tracker10TeV_p001-100_theta89_10k_sim.slcio"  --Output_REC.LCIOOutputFile="muonGun_tracker10TeV_p001-100_theta89_10k_reco.slcio" actsseedckf_steer.xml
