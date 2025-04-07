9aa35cc7d5eab9586147a414134df1443f40771a
Sanghyun Ko <sang.hyun.ko@cern.ch>
2025-04-08 00:49:37 +0900

    switch to edm4hep::SimCalorimeterHit for DRC to store position

--- plugins/Geant4Output2EDM4hep_DRC.cpp
+++ plugins/Geant4Output2EDM4hep_DRC.cpp
@@ -11,7 +11,6 @@
 #include <edm4hep/CaloHitContributionCollection.h>
 #include <edm4hep/EDM4hepVersion.h>
 #include <edm4hep/MCParticleCollection.h>
-#include <edm4hep/RawCalorimeterHitCollection.h>
 #include <edm4hep/RawTimeSeriesCollection.h>
 #include <edm4hep/SimCalorimeterHitCollection.h>
 #include <edm4hep/SimTrackerHitCollection.h>
@@ -53,7 +52,7 @@ namespace sim {
     using trackermap_t = std::map<std::string, edm4hep::SimTrackerHitCollection>;
     using calorimeterpair_t = std::pair<edm4hep::SimCalorimeterHitCollection, edm4hep::CaloHitContributionCollection>;
     using calorimetermap_t = std::map<std::string, calorimeterpair_t>;
-    using drcalopair_t = std::pair<edm4hep::RawCalorimeterHitCollection,
+    using drcalopair_t = std::pair<edm4hep::SimCalorimeterHitCollection,
                                    edm4hep::RawTimeSeriesCollection>; // Required info for IDEA DRC sim hit
     using drcalomap_t = std::map<std::string, drcalopair_t>;          // Required info for IDEA DRC sim hit
     using drcaloWavmap_t = std::map<std::string, edm4hep::RawTimeSeriesCollection>;
@@ -607,7 +606,7 @@ void Geant4Output2EDM4hep_DRC::saveCollection(OutputContext<G4Event>& /*ctxt*/, 
       const Geant4DRCalorimeter::Hit* hit = coll->hit(i);
 
       // For DRC raw calo hit & raw time series
-      auto rawCaloHits = DRhits.first->create();
+      auto simCaloHits = DRhits.first->create();
       auto rawTimeStruct = DRhits.second->create();
       auto rawWaveStruct = DRwaves->create();
 
@@ -662,9 +661,13 @@ void Geant4Output2EDM4hep_DRC::saveCollection(OutputContext<G4Event>& /*ctxt*/, 
         rawWaveStruct.addToAdcCounts(count);
       }
 
-      rawCaloHits.setCellID(hit->cellID);
-      rawCaloHits.setAmplitude(hit->GetPhotonCount());
-      rawCaloHits.setTimeStamp(peakTime - 1 + static_cast<int>(timeStart / samplingT));
+      const auto& pos = hit->position;
+      simCaloHits.setCellID(hit->cellID);
+      // store the number of photon in place of energy
+      // will become energy after the conversion to ADC counts
+      // no hit contribution is stored
+      simCaloHits.setEnergy(static_cast<float>(hit->GetPhotonCount()));
+      simCaloHits.setPosition({float(pos.x() / CLHEP::mm), float(pos.y() / CLHEP::mm), float(pos.z() / CLHEP::mm)});
     }
     //-------------------------------------------------------------------
   } else {
