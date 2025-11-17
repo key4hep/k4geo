15df8b956aefa2836dd8698b789ef087788d6b2f
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2025-11-17 12:38:10 +0100

    [IDEA] reproduce scepcal light yields (#539)
    * Remove scepcal filter in IDEA_o2 steering file
    
    The edep1kev filter in the IDEA_o2 steering file created empty
    collections for the scepcal beucase optical photons were not treated
    with the custom SDAction. By removing such filters scepcal collections
    are populated.
    
    * Reproduce scepcal scintillating light yield
    
    So far the SCEPCAL SDAction was simply counting how many S photons were
    emitted in each crystal. This did not reproduce the expected light yield
    of 2000 p.e./GeV. Plus, the IDEA_o2 steering file was not registering
    the scintillation process, so unless modified, no S photons were created
    and corresponding collection was empty.
    Instead of including S photons (and killing them immediately), I
    calculate S signals from the ionizing energy deposit and include a
    poissonian smearing to reproduce on average the expected light yield.
    
    * Reproduce scepcal Cerenkov light yield
    
    The current approach let Geant4 produce Cerenkov photons and counts
    them. This leads an unrealistically high Cerenkov light yield of 67000 C
    counts / GeV. The expected Cerenkov light yield from test-beam data is
    around 100 C p.e. / GeV. This includes a simple Bernoulli trial to
    select the C photons contributing to the signal and reproduces on
    average the expected light yield.
    
    * Remove Cerenkov light from scepcal timing layer
    
    In this very thin section of LYSO crystals we are not going to readout
    Cerenkov light. This is an high light yield scintillator for precise
    timing measurements of mip-like particles, no Cerenkov light is
    involved. Removing Cerenkov light production inside this subdetector.
    
    * Reproduce scepcal timing layer light yield
    
    The timing layer LYSO light yield expected in 6000 p.e./MeV. The
    correspoding hit collections were empty because the scintillation
    process was not registered in the IDEA_o2 steering file. This commit
    calculates the expected scintillation photo-electrons using the energy
    deposit instead.
    
    * Move scepcal functions to SD classes
    
    Move functions to calculate scepcal signals inside the custom classes of
    the SDActions. Use CLHEP constants for MeV to GeV convertions. Identify
    Cerenkov process with Geant4 enum instead of integer.
    
    * clang-format scepcal SDActions

--- plugins/SCEPCal_MainSDAction.cpp
+++ plugins/SCEPCal_MainSDAction.cpp
@@ -6,8 +6,10 @@
 #include "DDG4/Geant4SensDetAction.inl"
 #include "G4EmProcessSubType.hh"
 #include "G4OpticalPhoton.hh"
+#include "G4Poisson.hh"
 #include "G4ProcessType.hh"
 #include "G4VProcess.hh"
+#include "Randomize.hh"
 #include "detectorSegmentations/SCEPCal_MainSegmentation_k4geo.h"
 
 namespace SCEPCal {
@@ -15,6 +17,26 @@ class SCEPCal_MainSDAction {
 public:
   std::size_t m_collectionID_scint{0};
   std::size_t m_collectionID_ceren{0};
+
+  double m_scintPhotoEleGeV{2000.0};   // default value
+  double m_cherenBinomialCut{0.00147}; // default value
+
+  // This function takes in input the ionizing energy deposit (in MeV) from a step in the main scepcal section
+  // and returns the corresponding number of sipm fired cells (or photo-electrons).
+  // It reproduces a scintillating light yield of 2000 p.e./GeV which is expected from test-beam data.
+  // The smearing is done by a single poissoninan sampling. De facto one must have two samplings:
+  // a poissonian sampling for light emission fluctuations and a Binomial sampling for light detection
+  // fluctuations. Thanks to the Poissonian thinning theorem this is equivalent to a single poissonian
+  // sampling reproducing on average the expected light yield.
+  int SmearSsignal(G4double edep) const {
+    return G4Poisson((edep / CLHEP::GeV) * m_scintPhotoEleGeV); // MeV->GeV
+  }
+
+  // This function is a Bernoully trial to decide wether a Cerenkov photon is detected or not
+  // in the scepcal. Cerenkov photons are created by Geant4 inside crystals, already taking
+  // into account their poissonian emission fluctuations, therefore we only need to add a
+  // Binomial sampling that reproduces the expected light yield of 100 p.e./GeV
+  bool SmearCsignal() const { return G4UniformRand() < m_cherenBinomialCut; }
 };
 } // namespace SCEPCal
 
@@ -27,6 +49,17 @@ namespace sim {
     m_collectionID = defineCollection<Geant4Calorimeter::Hit>("SCEPCal_MainEdep");
     m_userData.m_collectionID_scint = defineCollection<Geant4Calorimeter::Hit>("SCEPCal_MainScounts");
     m_userData.m_collectionID_ceren = defineCollection<Geant4Calorimeter::Hit>("SCEPCal_MainCcounts");
+  }
+
+  template <>
+  Geant4SensitiveAction<SCEPCal_MainSDAction>::Geant4SensitiveAction(Geant4Context* ctxt, const std::string& nam,
+                                                                     DetElement det, Detector& lcdd_ref)
+      : Geant4Sensitive(ctxt, nam, det, lcdd_ref), m_collectionID(0) {
+    initialize();
+    defineCollections();
+    InstanceCount::increment(this);
+    declareProperty("scintPhotoElePerGeV", m_userData.m_scintPhotoEleGeV = 2000.);
+    declareProperty("cherenBinomialCut", m_userData.m_cherenBinomialCut = 0.00147);
   }
 
   template <>
@@ -56,24 +89,38 @@ namespace sim {
       return hit;
     };
 
-    // edep hits
-    auto* hitedep = newOrExistingHitIn(m_collectionID);
-    hitedep->energyDeposit += edep;
+    // Do not fill edep and S hits if there is no ionizing energy deposit
+    //
+    if (edep > 0.) {
+      // edep hits
+      auto* hitedep = newOrExistingHitIn(m_collectionID);
+      hitedep->energyDeposit += edep;
 
-    // Scintillation and Cerenkov hits
+      // scintillation hits
+      auto* hitS = newOrExistingHitIn(m_userData.m_collectionID_scint);
+      auto Scount = m_userData.SmearSsignal(edep);
+      // The EDM4HEP converter divides this entry by CLHEP::GeV to save energies in GeV unit.
+      // Here, we are saving S counts so we multiply by CLHEP::GeV to have the correct number in the output file.
+      if (Scount > 0)
+        hitS->energyDeposit += Scount * CLHEP::GeV;
+    }
+
+    // Cerenkov hits
     if (track->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()) {
-      auto procName = track->GetCreatorProcess()->GetProcessName();
-      bool isCerenkov = (procName == "CerenkovPhys");
-      bool isScintillation = (procName == "ScintillationPhys");
-      if (!isCerenkov && !isScintillation)
-        return true;
-
-      if (track->GetCurrentStepNumber() == 1) {
-        auto* hitSC =
-            newOrExistingHitIn(isScintillation ? m_userData.m_collectionID_scint : m_userData.m_collectionID_ceren);
-        hitSC->energyDeposit += 1 / dd4hep::MeV;
+      auto procSubType = track->GetCreatorProcess()->GetProcessSubType();
+      bool isCerenkov = (procSubType == G4EmProcessSubType::fCerenkov);
+      if (!isCerenkov) {
         track->SetTrackStatus(fStopAndKill);
-      }
+        return true;
+      } else if (track->GetCurrentStepNumber() == 1) {
+        auto* hitC = newOrExistingHitIn(m_userData.m_collectionID_ceren);
+        // The EDM4HEP converter divides this entry by CLHEP::GeV to save energies in GeV unit.
+        // Here, we are saving C counts so we multiply by CLHEP::GeV to have the correct number in the output file.
+        if (m_userData.SmearCsignal())
+          hitC->energyDeposit += 1 * CLHEP::GeV;
+        track->SetTrackStatus(fStopAndKill);
+      } else
+        return true;
     }
     return true;
   }
