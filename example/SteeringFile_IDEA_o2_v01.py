6d646dac131bcbe67f209cc75df7eee88df627ca
andread3vita <andrea.de.vita@cern.ch>
2026-09-18 09:32:02 +0200

    Fix userOutputPlugin for IDEA o2

--- example/SteeringFile_IDEA_o2_v01.py
+++ example/SteeringFile_IDEA_o2_v01.py
@@ -466,23 +466,12 @@ SIM.outputConfig.forceLCIO = False
 ##       SIM.outputConfig.myExtension = '.csv'
 ##
 
+# Only use shared=True in MT mode to avoid double-save in ST mode
+from DDG4 import EventAction, Kernel
+shared = dd4hepSimulation.numberOfThreads > 1
+evt_root = EventAction(Kernel(), 'Geant4Output2ROOT/' + dd.outputFile, shared)
 
-def Geant4Output2EDM4hep_DRC_plugin(dd4hepSimulation):
-    from DDG4 import EventAction, Kernel
-
-    shared = dd4hepSimulation.numberOfThreads > 1
-    evt_root = EventAction(
-        Kernel(), "Geant4Output2EDM4hep_DRC/" + dd4hepSimulation.outputFile, shared
-    )
-    evt_root.Control = True
-    output = dd4hepSimulation.outputFile
-    evt_root.Output = output
-    evt_root.enableUI()
-    Kernel().eventAction().add(evt_root)
-    return None
-
-
-SIM.outputConfig.userOutputPlugin = Geant4Output2EDM4hep_DRC_plugin
+SIM.outputConfig.userOutputPlugin = evt_root
 
 ################################################################################
 ## Configuration for the Particle Handler/ MCTruth treatment
