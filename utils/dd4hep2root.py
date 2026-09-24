1c688030f6eb56a64885c59e09467afe6a2b33fb
pablo <74920898+apausa@users.noreply.github.com>
2026-09-24 16:07:19 +0200

    Fix bug in converter

--- utils/dd4hep2root.py
+++ utils/dd4hep2root.py
@@ -30,14 +30,17 @@ def convert(compact_files, out_path):
     import ROOT
 
     ROOT.gSystem.Load("libDDCore")
-    description = ROOT.dd4hep.Detector.getInstance()
-    for cfile in compact_files:
-        description.fromXML(cfile)
 
+    # Store predefined ROOT colors before loading the file
     predefinedColors = dict(
         (color.GetNumber(), color) for color in ROOT.gROOT.GetListOfColors() if color
     )
+
     cachedColors = dict((num, num) for num in predefinedColors)
+
+    description = ROOT.dd4hep.Detector.getInstance()
+    for cfile in compact_files:
+        description.fromXML(cfile)
 
     def mapColor(colorNumber):
         if colorNumber not in cachedColors:
